048cf148f5e57c78ee89347f66264aaec8cb836c
jacklee <chunhsiang@mds.com.tw>
2023-04-25 20:13:58 +0800

    2023/04/25-3

--- leetcode/editor/en/[43]Multiply Strings.java
+++ leetcode/editor/en/[43]Multiply Strings.java
@@ -9,22 +9,22 @@ class Solution {
         char[] c2 = num2.toCharArray();
         int len1 = c1.length;
         int len2 = c2.length;
-        int[] prod = new int[len1 + len2];
+        int[] arr = new int[len1 + len2];
         for (int i = len1 - 1; i >= 0; i--) {
             for (int j = len2 - 1; j >= 0; j--) {
                 int d1 = c1[i] - '0';
                 int d2 = c2[j] - '0';
-                prod[i + j + 1] += d1 * d2;
+                arr[i + j + 1] += d1 * d2;
             }
         }
         int carry = 0;
-        for (int i = prod.length - 1; i >= 0; i--) {
-            int temp = (prod[i] + carry) % 10;
-            carry = (prod[i] + carry) / 10;
-            prod[i] = temp;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int temp = (arr[i] + carry) % 10;
+            carry = (arr[i] + carry) / 10;
+            arr[i] = temp;
         }
         StringBuilder sb = new StringBuilder();
-        for (int num : prod) {
+        for (int num : arr) {
             sb.append(num);
         }
         while (sb.length() > 0 && sb.charAt(0) == '0') {
