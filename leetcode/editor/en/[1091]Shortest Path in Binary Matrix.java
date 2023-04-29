783c7ec9c869fffc01e0b627f415dd3fed97a7ca
jacklee <chunhsiang@mds.com.tw>
2023-04-29 12:28:18 +0800

    2023/04/27 - 3

--- leetcode/editor/en/[1091]Shortest Path in Binary Matrix.java
+++ leetcode/editor/en/[1091]Shortest Path in Binary Matrix.java
@@ -4,30 +4,33 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
 
     public int shortestPathBinaryMatrix(int[][] grid) {
+        int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
         int n = grid.length;
-        if (grid[0][0] != 0 || grid[n - 1][n - 1] != 0) {
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) {
             return -1;
         }
-        Deque<int[]> dq = new ArrayDeque<>();
         boolean[][] isVisited = new boolean[n][n];
+        Deque<int[]> dq = new ArrayDeque<>();
         dq.add(new int[]{0, 0, 1});
         isVisited[0][0] = true;
         while (!dq.isEmpty()) {
-            int[] curr = dq.poll();
-            if (curr[0] == n - 1 && curr[1] == n - 1) {
-                return curr[2];
-            }
-            for (int[] dir : dirs) {
-                int x = curr[0] + dir[0];
-                int y = curr[1] + dir[1];
-                if (x < 0 || x >= n || y < 0 || y >= n || grid[x][y] == 1 || isVisited[x][y]) {
-                    continue;
+            int size = dq.size();
+            for (int i = 0; i < size; i++) {
+                int[] curr = dq.poll();
+                if (curr[0] == n - 1 && curr[1] == n - 1) {
+                    return curr[2];
                 }
-                isVisited[x][y] = true;
-                dq.add(new int[]{x, y, curr[2] + 1});
+                for (int[] dir : dirs) {
+                    int x = curr[0] + dir[0];
+                    int y = curr[0] + dir[1];
+                    if (x < 0 || x >= n || y < 0 || y >= n || grid[x][y] == 1 || isVisited[x][y]) {
+                        continue;
+                    }
+                    dq.add(new int[]{x, y, curr[2] + 1});
+                    isVisited[x][y] = true;
+                }
             }
         }
         return -1;
