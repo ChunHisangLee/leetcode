0ea2bda05e726c620333074e682612d2da8099c8
ChunHisangLee <goldrushkl168@gmail.com>
2023-05-02 22:31:49 +0800

    2023/05/02 home - 3

--- leetcode/editor/en/[1376]Time Needed to Inform All Employees.java
+++ leetcode/editor/en/[1376]Time Needed to Inform All Employees.java
@@ -2,7 +2,21 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
-        
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            if (manager[i] != 0) {
+                max = Math.max(max, dfs(i, manager, informTime));
+            }
+        }
+        return max;
+    }
+
+    public int dfs(int i, int[] manager, int[] informTime) {
+        if (manager[i] != -1) {
+            informTime[i] += dfs(manager[i], manager, informTime);
+            manager[i] = -1;
+        }
+        return informTime[i];
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
--- leetcode/editor/en/[802]Find Eventual Safe States.java
+++ leetcode/editor/en/[802]Find Eventual Safe States.java
@@ -1,8 +1,37 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
+/*
+還不會
+ "white-gray-black" DFS algorithm
+ */
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        
+        int n = graph.length;
+        int[] arr = new int[n];
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (dfs(i, arr, graph)) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+
+    public boolean dfs(int i, int[] arr, int[][] graph) {
+        if (arr[i] > 0) {
+            return arr[i] == 2;
+        }
+        arr[i] = 1;
+        for (int num : graph[i]) {
+            if (arr[num] == 2) {
+                continue;
+            }
+            if (arr[num] == 1 || !dfs(num, arr, graph)) {
+                return false;
+            }
+        }
+        arr[i] = 2;
+        return true;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
