d80c5f86227bb8a372f7cef5a7a43eb16d4046f7
jacklee <chunhsiang@mds.com.tw>
2023-05-10 11:47:39 +0800

    2023/05/10 -2

--- leetcode/editor/en/[173]Binary Search Tree Iterator.java
+++ leetcode/editor/en/[173]Binary Search Tree Iterator.java
@@ -2,34 +2,40 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class BSTIterator {
-
+    List<Integer>list =new ArrayList<>();
+    int index=0;
     public BSTIterator(TreeNode root) {
-
+        inOrder(root);
     }
 
     public void inOrder(TreeNode node) {
-
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        list.add(node.val);
+        inOrder(node.right);
     }
 
     public int next() {
-
+        return list.get(index++);
     }
 
     public boolean hasNext() {
-
+        return list.size() > index ? true : false;
     }
 }
 
--- leetcode/editor/en/[1845]Seat Reservation Manager.java
+++ leetcode/editor/en/[1845]Seat Reservation Manager.java
@@ -1,17 +1,21 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class SeatManager {
+    PriorityQueue<Integer> pq;
 
     public SeatManager(int n) {
-        
+        pq = new PriorityQueue<>();
+        for (int i = 1; i <= n; i++) {
+            pq.add(i);
+        }
     }
-    
+
     public int reserve() {
-        
+        return pq.poll();
     }
-    
+
     public void unreserve(int seatNumber) {
-        
+        pq.add(seatNumber);
     }
 }
 
