b04200c865b0d9c1cb2796f79bef4b6851560684
jacklee <chunhsiang@mds.com.tw>
2023-05-11 11:27:12 +0800

    2023/05/11 -1

---
+++ leetcode/editor/en/[1035]Uncrossed Lines.java
@@ -0,0 +1,20 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (nums1[i] == nums2[j]) {
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
