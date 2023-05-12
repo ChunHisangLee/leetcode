aca37b7f096217a9091e872b0b5d43e53e7a6ea0
jacklee <chunhsiang@mds.com.tw>
2023-05-12 16:47:35 +0800

    2023/05/12 -3

--- leetcode/editor/en/[227]Basic Calculator II.java
+++ leetcode/editor/en/[227]Basic Calculator II.java
@@ -3,12 +3,41 @@
         2023-01-31 14:18:14
 
 //leetcode submit region begin(Prohibit modification and deletion)
-/*
-還不會
- */
 class Solution {
     public int calculate(String s) {
-
+        int n = s.length();
+        int curr = 0;
+        int lastNum = 0;
+        int res = 0;
+        char operator = '+';
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                curr = curr * 10 + (c - '0');
+            }
+            if ((!Character.isDigit(c) && c != ' ') || i == n - 1) {
+                switch (operator) {
+                    case '+':
+                        res += lastNum;
+                        lastNum = curr;
+                        break;
+                    case '-':
+                        res += lastNum;
+                        lastNum = -curr;
+                        break;
+                    case '*':
+                        lastNum *= curr;
+                        break;
+                    case '/':
+                        lastNum /= curr;
+                        break;
+                }
+                operator = c;
+                curr = 0;
+            }
+        }
+        res += lastNum;
+        return res;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
