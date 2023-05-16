cbe01799bb5e6932e55ead3ea9e0d7561cc07f6c
jacklee <chunhsiang@mds.com.tw>
2023-05-16 21:16:24 +0800

    2023/05/15 -3

--- leetcode/editor/en/[1584]Min Cost to Connect All Points.java
+++ leetcode/editor/en/[1584]Min Cost to Connect All Points.java
@@ -6,31 +6,61 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
         int n = points.length;
-        PriorityQueue<Pair<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> (a.getKey() - b.getKey()));
-        boolean[] inMST = new boolean[n];
-        pq.add(new Pair(0, 0));
-        int mstCost = 0;
-        int edgesUsed = 0;
-        while (edgesUsed < n) {
-            Pair<Integer, Integer> topElement = pq.poll();
-            int weight = topElement.getKey();
-            int currNode = topElement.getValue();
-            if (inMST[currNode]) {
-                continue;
-            }
-            inMST[currNode] = true;
-            mstCost += weight;
-            edgesUsed++;
-            for (int nextNode = 0; nextNode < n; ++nextNode) {
-                if (!inMST[nextNode]) {
-                    int nextWeight = Math.abs(points[currNode][0] - points[nextNode][0]) +
-                            Math.abs(points[currNode][1] - points[nextNode][1]);
-
-                    pq.add(new Pair(nextWeight, nextNode));
-                }
+        List<int[]> list = new ArrayList<>();
+        PriorityQueue<Edge> pq = new PriorityQueue<>((a, b) -> a.dist - b.dist);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                pq.add(new Edge(i, j, points[i], points[j]));
             }
         }
-        return mstCost;
+        UnionFind uf = new UnionFind(n);
+        int res = 0;
+        int count = 1;
+        while (count < n) {
+            Edge curr = pq.poll();
+            if (uf.union(curr.x, curr.y)) {
+                res += curr.dist;
+                count++;
+            }
+        }
+        return res;
+    }
+}
+
+class UnionFind {
+    int[] group;
+
+    public UnionFind(int n) {
+        group = new int[n];
+        for (int i = 0; i < n; i++) {
+            group[i] = i;
+        }
+    }
+
+    public int find(int x) {
+        return group[x] == x ? x : (group[x] = find(group[x]));
+    }
+
+    public boolean union(int i, int j) {
+        int x = find(i);
+        int y = find(j);
+        if (x == y) {
+            return false;
+        }
+        group[x] = y;
+        return true;
+    }
+}
+
+class Edge {
+    int x;
+    int y;
+    int dist;
+
+    public Edge(int x, int y, int[] xPoint, int[] yPoint) {
+        this.x = x;
+        this.y = y;
+        dist = Math.abs(xPoint[0] - yPoint[0]) + Math.abs(xPoint[1] - yPoint[1]);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
@@ -39,18 +69,18 @@ class Solution {
     public int minCostConnectPoints(int[][] points) {
         int n = points.length;
         List<int[]> list = new ArrayList<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 int len = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
-                list.add(new int[]{i, j, len});
+                pq.add(new int[]{i, j, len});
             }
         }
-        Collections.sort(list, (a, b) -> a[2] - b[2]);
         UnionFind uf = new UnionFind(n);
         int res = 0;
-        int count = 0;
-        for (int i = 0; i < list.size() && count < n - 1; i++) {
-            int[] curr = list.get(i);
+        int count = 1;
+        while (count < n) {
+            int[] curr = pq.poll();
             if (uf.union(curr[0], curr[1])) {
                 res += curr[2];
                 count++;
