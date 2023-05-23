d5c0c02080119f057f074e0f2c2decb5c5b35abc
jacklee <chunhsiang@mds.com.tw>
2023-05-23 16:23:03 +0800

    2023/05/23 office -2

--- leetcode/editor/en/[163]Missing Ranges.java
+++ leetcode/editor/en/[163]Missing Ranges.java
@@ -6,24 +6,23 @@
 class Solution {
     public List<List<Integer>> findMissingRanges(int[] nums, int lower, int upper) {
         int n = nums.length;
-        List<List<Integer>> missingRanges = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
         if (n == 0) {
-            missingRanges.add(Arrays.asList(lower, upper));
-            return missingRanges;
+            res.add(Arrays.asList(lower, upper));
+            return res;
         }
         if (lower < nums[0]) {
-            missingRanges.add(Arrays.asList(lower, nums[0] - 1));
+            res.add(Arrays.asList(lower, nums[0] - 1));
         }
         for (int i = 0; i < n - 1; i++) {
-            if (nums[i + 1] - nums[i] <= 1) {
-                continue;
+            if (nums[i + 1] - nums[i] > 1) {
+                res.add(Arrays.asList(nums[i] + 1, nums[i + 1] - 1));
             }
-            missingRanges.add(Arrays.asList(nums[i] + 1, nums[i + 1] - 1));
         }
         if (upper > nums[n - 1]) {
-            missingRanges.add(Arrays.asList(nums[n - 1] + 1, upper));
+            res.add(Arrays.asList(nums[n - 1] + 1, upper));
         }
-        return missingRanges;
+        return res;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
