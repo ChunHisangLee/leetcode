0e365e475bfddfece6baf0ba34520aab941f80cc
jacklee <chunhsiang@mds.com.tw>
2023-05-24 23:45:12 +0800

    2023/05/24 home -2

--- leetcode/editor/en/[841]Keys and Rooms.java
+++ leetcode/editor/en/[841]Keys and Rooms.java
@@ -4,16 +4,16 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    List<List<Integer>> res;
+    List<List<Integer>> rooms;
     int[] arr;
     int count;
     int n;
 
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        this.rooms = rooms;
         n = rooms.size();
         arr = new int[n];
         count = n;
-        res = rooms;
         dfs(0);
         return count == 0;
     }
@@ -21,7 +21,7 @@ class Solution {
     public void dfs(int i) {
         arr[i] = 1;
         count--;
-        List<Integer> list = res.get(i);
+        List<Integer> list = rooms.get(i);
         for (int key : list) {
             if (arr[key] == 0) {
                 dfs(key);
