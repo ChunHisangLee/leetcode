252438ceb9591cf77d3956b1203aadd6331da88a
jacklee <chunhsiang@mds.com.tw>
2023-05-25 18:43:00 +0800

    2023/05/25 office - 5

--- leetcode/editor/en/[1466]Reorder Routes to Make All Paths Lead to the City Zero.java
+++ leetcode/editor/en/[1466]Reorder Routes to Make All Paths Lead to the City Zero.java
@@ -1,8 +1,91 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    int count = 0;
 
     public int minReorder(int n, int[][] connections) {
+        List<List<int[]>> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(new ArrayList<>());
+        }
+        for (int[] edge : connections) {
+            list.get(edge[0]).add(new int[]{edge[1], 1});
+            list.get(edge[1]).add(new int[]{edge[0], 0});
+        }
+        boolean[] isVisited = new boolean[n];
+        dfs(list, isVisited, 0);
+        return count;
+    }
 
+    public void dfs(List<List<int[]>> list, boolean[] isVisited, int curr) {
+        isVisited[curr] = true;
+        for (int[] num : list.get(curr)) {
+            if (!isVisited[num[0]]) {
+                count += num[1];
+                dfs(list, isVisited, num[0]);
+            }
+        }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
+/*
+DFS:
+class Solution {
+    int count = 0;
+
+    public int minReorder(int n, int[][] connections) {
+        List<List<int[]>> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(new ArrayList<>());
+        }
+        for (int[] edge : connections) {
+            list.get(edge[0]).add(new int[]{edge[1], 1});
+            list.get(edge[1]).add(new int[]{edge[0], 0});
+        }
+        boolean[] isVisited = new boolean[n];
+        dfs(list, isVisited, 0);
+        return count;
+    }
+
+    public void dfs(List<List<int[]>> list, boolean[] isVisited, int curr) {
+        isVisited[curr] = true;
+        for (int[] num : list.get(curr)) {
+            if (!isVisited[num[0]]) {
+                count += num[1];
+                dfs(list, isVisited, num[0]);
+            }
+        }
+    }
+}
+
+
+BFS:
+class Solution {
+    int count = 0;
+
+    public int minReorder(int n, int[][] connections) {
+        List<List<int[]>> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(new ArrayList<>());
+        }
+        for (int[] edge : connections) {
+            list.get(edge[0]).add(new int[]{edge[1], 1});
+            list.get(edge[1]).add(new int[]{edge[0], 0});
+        }
+        Deque<Integer> dq = new ArrayDeque<>();
+        boolean[] isVisited = new boolean[n];
+        int res = 0;
+        dq.add(0);
+        while (!dq.isEmpty()) {
+            int curr = dq.poll();
+            isVisited[curr] = true;
+            for (int[] num : list.get(curr)) {
+                if (!isVisited[num[0]]) {
+                    res += num[1];
+                    dq.add(num[0]);
+                }
+            }
+        }
+        return res;
+    }
+}
+ */
