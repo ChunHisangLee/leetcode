dc7586ac870e3eff0e6f16f1f91d20f994203730
jacklee <chunhsiang@mds.com.tw>
2023-05-29 13:13:50 +0800

    2023/05/29 home - 4

--- leetcode/editor/en/[433]Minimum Genetic Mutation.java
+++ leetcode/editor/en/[433]Minimum Genetic Mutation.java
@@ -7,9 +7,13 @@ class Solution {
     public int minMutation(String startGene, String endGene, String[] bank) {
         char[] gene = new char[]{'A', 'C', 'G', 'T'};
         Set<String> set = new HashSet<>(Arrays.asList(bank));
+        if (!set.contains(endGene)) {
+            return -1;
+        }
         Set<String> isVisited = new HashSet<>();
         Deque<String> dq = new ArrayDeque<>();
         dq.add(startGene);
+        isVisited.add(startGene);
         int count = 0;
         while (!dq.isEmpty()) {
             int size = dq.size();
@@ -24,7 +28,10 @@ class Solution {
                     for (char g : gene) {
                         c[j] = g;
                         String str = String.valueOf(c);
-                        if (!isVisited.contains(str) && set.contains(str)) {
+                        if (!set.contains(str)) {
+                            continue;
+                        }
+                        if (!isVisited.contains(str)) {
                             dq.add(str);
                             isVisited.add(str);
                         }
