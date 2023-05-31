5f1c691109f92c9411b0ccb3ca4726bfb80139f6
jacklee <chunhsiang@mds.com.tw>
2023-05-31 13:56:36 +0800

    2023/05/31 office - 2

--- leetcode/editor/en/[1020]Number of Enclaves.java
+++ leetcode/editor/en/[1020]Number of Enclaves.java
@@ -1,52 +1,18 @@
-/**
- * <p>You are given an <code>m x n</code> binary matrix <code>grid</code>, where <code>0</code> represents a sea cell and <code>1</code> represents a land cell.</p>
- *
- * <p>A <strong>move</strong> consists of walking from one land cell to another adjacent (<strong>4-directionally</strong>) land cell or walking off the boundary of the <code>grid</code>.</p>
- *
- * <p>Return <em>the number of land cells in</em> <code>grid</code> <em>for which we cannot walk off the boundary of the grid in any number of <strong>moves</strong></em>.</p>
- *
- * <p>&nbsp;</p>
- * <p><strong class="example">Example 1:</strong></p>
- * <img alt="" src="https://assets.leetcode.com/uploads/2021/02/18/enclaves1.jpg" style="width: 333px; height: 333px;" />
- * <pre>
- * <strong>Input:</strong> grid = [[0,0,0,0],[1,0,1,0],[0,1,1,0],[0,0,0,0]]
- * <strong>Output:</strong> 3
- * <strong>Explanation:</strong> There are three 1s that are enclosed by 0s, and one 1 that is not enclosed because its on the boundary.
- * </pre>
- *
- * <p><strong class="example">Example 2:</strong></p>
- * <img alt="" src="https://assets.leetcode.com/uploads/2021/02/18/enclaves2.jpg" style="width: 333px; height: 333px;" />
- * <pre>
- * <strong>Input:</strong> grid = [[0,1,1,0],[0,0,1,0],[0,0,1,0],[0,0,0,0]]
- * <strong>Output:</strong> 0
- * <strong>Explanation:</strong> All 1s are either on the boundary or can reach the boundary.
- * </pre>
- *
- * <p>&nbsp;</p>
- * <p><strong>Constraints:</strong></p>
- *
- * <ul>
- * <li><code>m == grid.length</code></li>
- * <li><code>n == grid[i].length</code></li>
- * <li><code>1 &lt;= m, n &lt;= 500</code></li>
- * <li><code>grid[i][j]</code> is either <code>0</code> or <code>1</code>.</li>
- * </ul>
- *
- * <div><div>Related Topics</div><div><li>Array</li><li>Depth-First Search</li><li>Breadth-First Search</li><li>Union Find</li><li>Matrix</li></div></div><br><div><li>👍 2399</li><li>👎 43</li></div>
- */
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    int[][] grid;
     int m;
     int n;
 
     public int numEnclaves(int[][] grid) {
+        this.grid = grid;
         m = grid.length;
         n = grid[0].length;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == 0 || i == m - 1 || j == 0 || j == n - 1 && grid[i][j] == 1) {
-                    dfs(grid, i, j);
+                if ((i == 0 || j == 0 || i == m - 1 || j == n - 1) && grid[i][j] == 1) {
+                    dfs(i, j);
                 }
             }
         }
@@ -59,15 +25,15 @@ class Solution {
         return count;
     }
 
-    public void dfs(int[][] grid, int x, int y) {
+    public void dfs(int x, int y) {
         if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] == 0) {
             return;
         }
         grid[x][y] = 0;
-        dfs(grid, x + 1, y);
-        dfs(grid, x - 1, y);
-        dfs(grid, x, y + 1);
-        dfs(grid, x, y - 1);
+        dfs(x + 1, y);
+        dfs(x - 1, y);
+        dfs(x, y + 1);
+        dfs(x, y - 1);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
