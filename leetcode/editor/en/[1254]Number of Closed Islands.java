24e651cce3101cb538dde46bc1db00c99f5fcf16
jacklee <chunhsiang@mds.com.tw>
2023-06-01 11:07:07 +0800

    2023/06/01 office - 2

--- leetcode/editor/en/[1254]Number of Closed Islands.java
+++ leetcode/editor/en/[1254]Number of Closed Islands.java
@@ -1,88 +1,42 @@
-/**
- * <p>Given a 2D&nbsp;<code>grid</code> consists of <code>0s</code> (land)&nbsp;and <code>1s</code> (water).&nbsp; An <em>island</em> is a maximal 4-directionally connected group of <code><font face="monospace">0</font>s</code> and a <em>closed island</em>&nbsp;is an island <strong>totally</strong>&nbsp;(all left, top, right, bottom) surrounded by <code>1s.</code></p>
- *
- * <p>Return the number of <em>closed islands</em>.</p>
- *
- * <p>&nbsp;</p>
- * <p><strong class="example">Example 1:</strong></p>
- *
- * <p><img alt="" src="https://assets.leetcode.com/uploads/2019/10/31/sample_3_1610.png" style="width: 240px; height: 120px;" /></p>
- *
- * <pre>
- * <strong>Input:</strong> grid = [[1,1,1,1,1,1,1,0],[1,0,0,0,0,1,1,0],[1,0,1,0,1,1,1,0],[1,0,0,0,0,1,0,1],[1,1,1,1,1,1,1,0]]
- * <strong>Output:</strong> 2
- * <strong>Explanation:</strong>
- * Islands in gray are closed because they are completely surrounded by water (group of 1s).</pre>
- *
- * <p><strong class="example">Example 2:</strong></p>
- *
- * <p><img alt="" src="https://assets.leetcode.com/uploads/2019/10/31/sample_4_1610.png" style="width: 160px; height: 80px;" /></p>
- *
- * <pre>
- * <strong>Input:</strong> grid = [[0,0,1,0,0],[0,1,0,1,0],[0,1,1,1,0]]
- * <strong>Output:</strong> 1
- * </pre>
- *
- * <p><strong class="example">Example 3:</strong></p>
- *
- * <pre>
- * <strong>Input:</strong> grid = [[1,1,1,1,1,1,1],
- * &nbsp;              [1,0,0,0,0,0,1],
- * &nbsp;              [1,0,1,1,1,0,1],
- * &nbsp;              [1,0,1,0,1,0,1],
- * &nbsp;              [1,0,1,1,1,0,1],
- * &nbsp;              [1,0,0,0,0,0,1],
- * [1,1,1,1,1,1,1]]
- * <strong>Output:</strong> 2
- * </pre>
- *
- * <p>&nbsp;</p>
- * <p><strong>Constraints:</strong></p>
- *
- * <ul>
- * <li><code>1 &lt;= grid.length, grid[0].length &lt;= 100</code></li>
- * <li><code>0 &lt;= grid[i][j] &lt;=1</code></li>
- * </ul>
- *
- * <div><div>Related Topics</div><div><li>Array</li><li>Depth-First Search</li><li>Breadth-First Search</li><li>Union Find</li><li>Matrix</li></div></div><br><div><li>👍 2907</li><li>👎 88</li></div>
- */
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    int[][] grid;
     int m;
     int n;
-    boolean isClosed;
 
     public int closedIsland(int[][] grid) {
+        this.grid = grid;
         m = grid.length;
         n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i == 0 || i == m - 1 || j == 0 || j == n - 1) && grid[i][j] == 0) {
+                    dfs(i, j);
+                }
+            }
+        }
         int count = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 0) {
-                    isClosed = true;
-                    dfs(grid, i, j);
-                    if (isClosed) {
-                        count++;
-                    }
+                    count++;
+                    dfs(i, j);
                 }
             }
         }
         return count;
     }
 
-    public void dfs(int[][] grid, int x, int y) {
+    public void dfs(int x, int y) {
         if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] == 1) {
             return;
         }
         grid[x][y] = 1;
-        if (x == 0 || x == m - 1 || y == 0 || y == n - 1) {
-            isClosed = false;
-        }
-        dfs(grid, x + 1, y);
-        dfs(grid, x - 1, y);
-        dfs(grid, x, y + 1);
-        dfs(grid, x, y - 1);
+        dfs(x + 1, y);
+        dfs(x - 1, y);
+        dfs(x, y + 1);
+        dfs(x, y - 1);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
