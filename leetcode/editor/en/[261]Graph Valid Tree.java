58e98af320acdba049ea21a8b260cd33f6737a75
jacklee <chunhsiang@mds.com.tw>
2023-06-04 17:13:06 +0800

    2023/06/04 home -1

--- leetcode/editor/en/[261]Graph Valid Tree.java
+++ leetcode/editor/en/[261]Graph Valid Tree.java
@@ -5,7 +5,45 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean validTree(int n, int[][] edges) {
+        if (edges.length != n - 1) {
+            return false;
+        }
+        UnionFind uf = new UnionFind(n);
+        for (int[] edge : edges) {
+            if (!uf.union(edge[0], edge[1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
 
+class UnionFind {
+    int[] group;
+    int count = 0;
+
+    public UnionFind(int n) {
+        group = new int[n];
+        for (int i = 0; i < n; i++) {
+            group[i] = i;
+        }
+    }
+
+    public int find(int x) {
+        return group[x] == x ? x : (group[x] = find(group[x]));
+    }
+
+    public boolean union(int i, int j) {
+        int x = find(i);
+        int y = find(j);
+        if (x == y) {
+            return false;
+        }
+        if (x != y) {
+            group[x] = y;
+        }
+        count++;
+        return true;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
