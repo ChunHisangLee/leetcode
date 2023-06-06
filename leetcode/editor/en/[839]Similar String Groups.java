5b9be218994525da56c40077a76897a95dffd7d2
jacklee <chunhsiang@mds.com.tw>
2023-06-06 18:22:39 +0800

    2023/06/06 office -1

--- leetcode/editor/en/[839]Similar String Groups.java
+++ leetcode/editor/en/[839]Similar String Groups.java
@@ -1,18 +1,78 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    public void bfs(int node, Map<Integer, List<Integer>> adj, boolean[] visit) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(node);
+        visit[node] = true;
+        while (!q.isEmpty()) {
+            node = q.poll();
+            if (!adj.containsKey(node)) {
+                continue;
+            }
+            for (int neighbor : adj.get(node)) {
+                if (!visit[neighbor]) {
+                    visit[neighbor] = true;
+                    q.offer(neighbor);
+                }
+            }
+        }
+    }
+
+    public boolean isSimilar(String s1, String s2) {
+        int diff = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                diff++;
+                if (diff > 2) {
+                    return false;
+                }
+            }
+        }
+        return diff != 1;
+    }
+
+    public int numSimilarGroups(String[] strs) {
+        int n = strs.length;
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        // Form the required graph from the given strings array.
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isSimilar(strs[i], strs[j])) {
+                    adj.computeIfAbsent(i, k -> new ArrayList<Integer>()).add(j);
+                    adj.computeIfAbsent(j, k -> new ArrayList<Integer>()).add(i);
+                }
+            }
+        }
+
+        boolean[] visit = new boolean[n];
+        int count = 0;
+        // Count the number of connected components.
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                bfs(i, adj, visit);
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+/*
+DFS: - 18ms
+class Solution {
     public int numSimilarGroups(String[] strs) {
         int n = strs.length;
         Map<Integer, List<Integer>> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 if (isSimilar(strs[i], strs[j])) {
-                    map.computeIfAbsent(i, k -> new ArrayList<Integer>()).add(j);
-                    map.computeIfAbsent(j, k -> new ArrayList<Integer>()).add(i);
+                    map.computeIfAbsent(i, k -> new ArrayList<>()).add(j);
+                    map.computeIfAbsent(j, k -> new ArrayList<>()).add(i);
                 }
             }
         }
-
         boolean[] isVisited = new boolean[n];
         int count = 0;
         for (int i = 0; i < n; i++) {
@@ -21,8 +81,20 @@ class Solution {
                 count++;
             }
         }
-
         return count;
+    }
+
+    public boolean isSimilar(String s1, String s2) {
+        int diff = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                diff++;
+                if (diff > 2) {
+                    return false;
+                }
+            }
+        }
+        return diff != 1;
     }
 
     public void dfs(int node, Map<Integer, List<Integer>> map, boolean[] isVisited) {
@@ -30,22 +102,106 @@ class Solution {
         if (!map.containsKey(node)) {
             return;
         }
-        for (int neighbor : map.get(node)) {
-            if (!isVisited[neighbor]) {
-                isVisited[neighbor] = true;
-                dfs(neighbor, map, isVisited);
+        for (int num : map.get(node)) {
+            if (!isVisited[num]) {
+                isVisited[num] = true;
+                dfs(num, map, isVisited);
             }
+        }
+    }
+}
+DFS: - 10ms
+class Solution {
+    public int numSimilarGroups(String[] strs) {
+        int n = strs.length;
+        boolean[] isVisited = new boolean[n];
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (!isVisited[i]) {
+                isVisited[i] = true;
+                dfs(strs[i], isVisited, strs);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean isSimilar(String s1, String s2) {
+        int diff = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                diff++;
+                if (diff > 2) {
+                    return false;
+                }
+            }
+        }
+        return diff != 1;
+    }
+
+    public void dfs(String str, boolean[] isVisited, String[] strs) {
+        for (int i = 0; i < strs.length; i++) {
+            if (!isVisited[i] && isSimilar(str, strs[i])) {
+                isVisited[i] = true;
+                dfs(strs[i], isVisited, strs);
+            }
+        }
+    }
+}
+
+
+
+Union Find:
+class Solution {
+    public int numSimilarGroups(String[] strs) {
+        int n = strs.length;
+        UnionFind uf = new UnionFind(n);
+        int count = n;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isSimilar(strs[i], strs[j]) && uf.find(i) != uf.find(j)) {
+                    count--;
+                    uf.union(i, j);
+                }
+            }
+        }
+        return count;
+    }
+
+    public boolean isSimilar(String s1, String s2) {
+        int diff = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                diff++;
+                if (diff > 2) {
+                    return false;
+                }
+            }
+        }
+        return diff != 1;
+    }
+}
+
+class UnionFind {
+    int[] group;
+
+    public UnionFind(int n) {
+        group = new int[n];
+        for (int i = 0; i < n; i++) {
+            group[i] = i;
         }
     }
 
-    public boolean isSimilar(String a, String b) {
-        int diff = 0;
-        for (int i = 0; i < a.length(); i++) {
-            if (a.charAt(i) != b.charAt(i)) {
-                diff++;
-            }
+    public int find(int x) {
+        return group[x] == x ? x : (group[x] = find(group[x]));
+    }
+
+    public void union(int i, int j) {
+        int x = find(i);
+        int y = find(j);
+        if (x != y) {
+            group[x] = y;
         }
-        return diff == 0 || diff == 2;
     }
 }
-//leetcode submit region end(Prohibit modification and deletion)
+ */
