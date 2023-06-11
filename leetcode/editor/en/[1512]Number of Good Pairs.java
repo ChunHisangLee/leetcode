bc9a24aa36f78201da50782bf7630c0de1dae841
jacklee <chunhsiang@mds.com.tw>
2023-06-11 14:41:45 +0800

    2023/06/11 home -2

---
+++ leetcode/editor/en/[1512]Number of Good Pairs.java
@@ -0,0 +1,52 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int[] arr = new int[101];
+        int count = 0;
+        for (int num : nums) {
+            arr[num]++;
+        }
+        for (int num : arr) {
+            if (num > 1) {
+                count += num * (num - 1) / 2;
+            }
+        }
+        return count;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+/*
+ - 0ms
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int[] arr = new int[101];
+        int count = 0;
+        for (int num : nums) {
+            arr[num]++;
+        }
+        for (int num : arr) {
+            if (num > 1) {
+                count += num * (num - 1) / 2;
+            }
+        }
+        return count;
+    }
+}
+
+- 1ms;
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
+ */
