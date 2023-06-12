7c78b8f67c854d386f3eaef1fcaaec40ee512b3f
jacklee <chunhsiang@mds.com.tw>
2023-06-12 23:04:22 +0800

    2023/06/12 home -4

---
+++ leetcode/editor/en/[292]Nim Game.java
@@ -0,0 +1,8 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
