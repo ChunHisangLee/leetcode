5244b986c1a7d3199aeb111eaff2ac1306228598
jacklee <chunhsiang@mds.com.tw>
2023-06-17 09:40:25 +0800

    2023/06/17 office -1

---
+++ leetcode/editor/en/[1187]Make Array Strictly Increasing.java
@@ -0,0 +1,68 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int makeArrayIncreasing(int[] arr1, int[] arr2) {
+        Map<Integer, Integer> dp = new HashMap<>();
+        dp.put(-1, 0);
+        Arrays.sort(arr2);
+        for (int a : arr1) {
+            Map<Integer, Integer> nextDp = new HashMap<>();
+            for (int val : dp.keySet()) {
+                int steps = dp.get(val);
+                if (a > val)
+                    nextDp.put(a, Math.min(nextDp.getOrDefault(a, Integer.MAX_VALUE), steps));
+                int i = getNum(arr2, val);
+                if (i < arr2.length)
+                    nextDp.put(arr2[i], Math.min(nextDp.getOrDefault(arr2[i], Integer.MAX_VALUE), steps + 1));
+            }
+            if (nextDp.isEmpty()) {
+                return -1;
+            }
+            dp = nextDp;
+        }
+        return Collections.min(dp.values());
+    }
+
+    private int getNum(int[] arr, int value) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            if (arr[mid] <= value) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+/*
+class Solution {
+    public int makeArrayIncreasing(int[] arr1, int[] arr2) {
+        Map<Integer, Integer> dp = new HashMap<>();
+        dp.put(-1, 0);
+        Arrays.sort(arr2);
+        for ( int a : arr1) {
+            Map<Integer, Integer> nextDp = new HashMap<>();
+            for ( int val : dp.keySet()) {
+                 int steps = dp.get(val);
+                if (a > val)
+                    nextDp.put(a, Math.min(nextDp.getOrDefault(a, Integer.MAX_VALUE), steps));
+                 int i = firstGreater(arr2, val);
+                if (i < arr2.length)
+                    nextDp.put(arr2[i], Math.min(nextDp.getOrDefault(arr2[i], Integer.MAX_VALUE), steps + 1));
+            }
+            if (nextDp.isEmpty())
+                return -1;
+            dp = nextDp;
+        }
+        return Collections.min(dp.values());
+    }
+    private int firstGreater(int[] A, int val) {
+         int i = Arrays.binarySearch(A, val + 1);
+        return i < 0 ? -(i + 1) : i;
+    }
+}
+ */
