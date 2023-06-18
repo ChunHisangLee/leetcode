94aca7089084d23bea788a6118cfc712f4f7102e
jacklee <chunhsiang@mds.com.tw>
2023-06-18 13:36:31 +0800

    2023/06/18 home -4

--- leetcode/editor/en/[374]Guess Number Higher or Lower.java
+++ leetcode/editor/en/[374]Guess Number Higher or Lower.java
@@ -17,12 +17,14 @@ public class Solution extends GuessGame {
     public int guessNumber(int n) {
         int left = 1;
         int right = n;
-        while (left < right) {
+        while (left <= right) {
             int mid = (left + right) >>> 1;
-            if (guess(mid) <= 0) {
-                right = mid;
-            } else {
+            if (guess(mid) == 0) {
+                return mid;
+            } else if (guess(mid) > 0) {
                 left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
         return left;
