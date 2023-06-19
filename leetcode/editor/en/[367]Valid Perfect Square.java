daa5e442d3f610a1aaf112fa85550459f3949636
jacklee <chunhsiang@mds.com.tw>
2023-06-19 18:04:08 +0800

    2023/06/19 office -1

--- leetcode/editor/en/[367]Valid Perfect Square.java
+++ leetcode/editor/en/[367]Valid Perfect Square.java
@@ -1,23 +1,23 @@
-69
-        Sqrt(x)
-        2022-12-28 17:29:33
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public int mySqrt(int x) {
-        long left = 1;
-        long right = x;
+    public boolean isPerfectSquare(int num) {
+        if (num < 2) {
+            return true;
+        }
+        long left = 0;
+        long right = num >> 1;
         while (left <= right) {
             long mid = (left + right) >> 1;
-            if (mid * mid == x) {
-                return (int) mid;
-            } else if (mid * mid < x) {
+            if (mid * mid == num) {
+                return true;
+            } else if (mid * mid < num) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
-        return (int) left - 1;
+        return false;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
--- leetcode/editor/en/[441]Arranging Coins.java
+++ leetcode/editor/en/[441]Arranging Coins.java
@@ -1,17 +1,14 @@
-69
-        Sqrt(x)
-        2022-12-28 17:29:33
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public int mySqrt(int x) {
-        long left = 1;
-        long right = x;
+    public int arrangeCoins(int n) {
+        long left = 0;
+        long right = n;
         while (left <= right) {
             long mid = (left + right) >> 1;
-            if (mid * mid == x) {
+            if ((mid) * (mid + 1) / 2 == n) {
                 return (int) mid;
-            } else if (mid * mid < x) {
+            } else if ((mid) * (mid + 1) / 2 < n) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
