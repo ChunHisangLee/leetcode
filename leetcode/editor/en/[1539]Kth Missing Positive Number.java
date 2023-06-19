d7a325bbd44b7ba6cd40e435e5a43b04d7c89b93
jacklee <chunhsiang@mds.com.tw>
2023-06-19 18:19:07 +0800

    2023/06/19 office -2

--- leetcode/editor/en/[1539]Kth Missing Positive Number.java
+++ leetcode/editor/en/[1539]Kth Missing Positive Number.java
@@ -1,61 +1,37 @@
-/**
- * <p>Given an array <code>arr</code> of positive integers sorted in a <strong>strictly increasing order</strong>, and an integer <code>k</code>.</p>
- *
- * <p>Return <em>the</em> <code>k<sup>th</sup></code> <em><strong>positive</strong> integer that is <strong>missing</strong> from this array.</em></p>
- *
- * <p>&nbsp;</p>
- * <p><strong class="example">Example 1:</strong></p>
- *
- * <pre>
- * <strong>Input:</strong> arr = [2,3,4,7,11], k = 5
- * <strong>Output:</strong> 9
- * <strong>Explanation: </strong>The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5<sup>th</sup>&nbsp;missing positive integer is 9.
- * </pre>
- *
- * <p><strong class="example">Example 2:</strong></p>
- *
- * <pre>
- * <strong>Input:</strong> arr = [1,2,3,4], k = 2
- * <strong>Output:</strong> 6
- * <strong>Explanation: </strong>The missing positive integers are [5,6,7,...]. The 2<sup>nd</sup> missing positive integer is 6.
- * </pre>
- *
- * <p>&nbsp;</p>
- * <p><strong>Constraints:</strong></p>
- *
- * <ul>
- * <li><code>1 &lt;= arr.length &lt;= 1000</code></li>
- * <li><code>1 &lt;= arr[i] &lt;= 1000</code></li>
- * <li><code>1 &lt;= k &lt;= 1000</code></li>
- * <li><code>arr[i] &lt; arr[j]</code> for <code>1 &lt;= i &lt; j &lt;= arr.length</code></li>
- * </ul>
- *
- * <p>&nbsp;</p>
- * <p><strong>Follow up:</strong></p>
- *
- * <p>Could you solve this problem in less than O(n) complexity?</p>
- *
- * <div><div>Related Topics</div><div><li>Array</li><li>Binary Search</li></div></div><br><div><li>👍 5210</li><li>👎 333</li></div>
- */
 
 //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            if (arr[mid] - mid - 1 < k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left + k;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+/*
 class Solution {
     public int findKthPositive(int[] arr, int k) {
         Set<Integer> set = new HashSet<>();
         for (int num : arr) {
             set.add(num);
         }
-        int res = 0;
         int count = 0;
-        for (int i = 1; i <= set.size() + k; i++) {
-            if (!set.contains(i)) {
+        int key = 1;
+        while (count < k) {
+            if (!set.contains(key)) {
                 count++;
             }
-            if (count == k) {
-                return i;
-            }
+            key++;
         }
-        return -1;
+        return key - 1;
     }
 }
-//leetcode submit region end(Prohibit modification and deletion)
+ */
