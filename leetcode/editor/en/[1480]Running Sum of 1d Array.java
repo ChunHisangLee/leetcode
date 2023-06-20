af37b22884edb0b13a0ff7444dae99be4922f18f
jacklee <chunhsiang@mds.com.tw>
2023-06-20 10:08:28 +0800

    2023/06/20 office -1

--- leetcode/editor/en/[1480]Running Sum of 1d Array.java
+++ leetcode/editor/en/[1480]Running Sum of 1d Array.java
@@ -5,10 +5,14 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] runningSum(int[] nums) {
-        for (int i = 1; i < nums.length; i++) {
-            nums[i] += nums[i - 1];
+        int n = nums.length;
+        int[] preSum = new int[n + 1];
+        int[] arr = new int[n];
+        for (int i = 1; i <= n; i++) {
+            preSum[i] = preSum[i - 1] + nums[i - 1];
+            arr[i - 1] = preSum[i];
         }
-        return nums;
+        return arr;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
