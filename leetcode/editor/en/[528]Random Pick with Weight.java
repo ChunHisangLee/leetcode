99b881179b718177b1b4c4e82132944cd8aa6efe
jacklee <chunhsiang@mds.com.tw>
2023-06-24 07:03:51 +0800

    2023/06/24 home -1

--- leetcode/editor/en/[528]Random Pick with Weight.java
+++ leetcode/editor/en/[528]Random Pick with Weight.java
@@ -1,13 +1,32 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    int[] prefixSum;
+    int total;
 
     public Solution(int[] w) {
-        
+        int n = w.length;
+        prefixSum = new int[n];
+        total = 0;
+        for (int i = 0; i < n; i++) {
+            total += w[i];
+            prefixSum[i] = total;
+        }
     }
-    
+
     public int pickIndex() {
-        
+        double target = total * Math.random();
+        int left = 0;
+        int right = prefixSum.length;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            if (prefixSum[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
     }
 }
 
