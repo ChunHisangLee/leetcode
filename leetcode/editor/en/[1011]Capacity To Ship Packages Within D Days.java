5f8097367e2e47fb92bde4ca7086257c9b7a123f
jacklee <chunhsiang@mds.com.tw>
2023-06-24 14:12:19 +0800

    2023/06/24 home -3

--- leetcode/editor/en/[1011]Capacity To Ship Packages Within D Days.java
+++ leetcode/editor/en/[1011]Capacity To Ship Packages Within D Days.java
@@ -6,28 +6,36 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
         int left = 0;
-        int right = Integer.MAX_VALUE;
-        for (int weight : weights) {
-            left = Math.max(left, weight);
+        int right = 0;
+        for (int num : weights) {
+            left = Math.max(num, left);
+            right += num;
         }
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            int count = 1;
-            int carry = 0;
-            for (int weight : weights) {
-                carry += weight;
-                if (carry > mid) {
-                    count++;
-                    carry = weight;
-                }
-            }
-            if (count > days) {
+        while (left <= right) {
+            int mid = (left + right) >>> 1;
+            if (isCheck(weights, mid, days)) {
                 left = mid + 1;
             } else {
-                right = mid;
+                right = mid - 1;
             }
         }
-        return right;
+        return left;
+    }
+
+    private boolean isCheck(int[] weights, int mid, int days) {
+        int count = 1;
+        int carry = 0;
+        for (int num : weights) {
+            carry += num;
+            if (carry > mid) {
+                count++;
+                if (count > days) {
+                    return true;
+                }
+                carry = num;
+            }
+        }
+        return false;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
