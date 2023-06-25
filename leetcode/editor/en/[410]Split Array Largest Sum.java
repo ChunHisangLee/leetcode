7ad46b6ec29e6361c48a4283c03fc3b4ed10a7b9
jacklee <chunhsiang@mds.com.tw>
2023-06-25 23:37:35 +0800

    2023/06/25 home -2

--- leetcode/editor/en/[410]Split Array Largest Sum.java
+++ leetcode/editor/en/[410]Split Array Largest Sum.java
@@ -15,29 +15,28 @@ class Solution {
         int right = sum;
         int res = 0;
         while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (check(mid, nums) <= k) {
+            int mid = (left + right) >>> 1;
+            if (isCheck(nums, mid, k)) {
+                left = mid + 1;
+            } else {
                 right = mid - 1;
                 res = mid;
-            } else {
-                left = mid + 1;
             }
         }
         return res;
     }
 
-    private int check(int k, int[] nums) {
-        int curr = 0;
-        int count = 0;
+    private boolean isCheck(int[] nums, int mid, int k) {
+        int sum = 0;
+        int count = 1;
         for (int num : nums) {
-            if (curr + num <= k) {
-                curr += num;
-            } else {
-                curr = num;
+            sum += num;
+            if (sum > mid) {
                 count++;
+                sum = num;
             }
         }
-        return count + 1;
+        return count > k;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
