62585c441b87522491f7d9b531ad43afc8dbf23f
jacklee <chunhsiang@mds.com.tw>
2023-06-26 17:25:22 +0800

    2023/06/26 office -5

--- leetcode/editor/en/[1114]Print in Order.java
+++ leetcode/editor/en/[1114]Print in Order.java
@@ -1,27 +1,45 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Foo {
-
+    private int currNum;
     public Foo() {
-        
+        currNum=1;
     }
 
     public void first(Runnable printFirst) throws InterruptedException {
-        
-        // printFirst.run() outputs "first". Do not change or remove this line.
-        printFirst.run();
+        synchronized (this) {
+            while (currNum != 1) {
+                wait();
+            }
+            // printFirst.run() outputs "first". Do not change or remove this line.
+            printFirst.run();
+            currNum++;
+            notifyAll();
+        }
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-        
-        // printSecond.run() outputs "second". Do not change or remove this line.
-        printSecond.run();
+        synchronized (this) {
+            while (currNum != 2) {
+                wait();
+            }
+            // printSecond.run() outputs "second". Do not change or remove this line.
+            printSecond.run();
+            currNum++;
+            notifyAll();
+        }
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-        
-        // printThird.run() outputs "third". Do not change or remove this line.
-        printThird.run();
+        synchronized (this) {
+            while (currNum != 3) {
+                wait();
+            }
+            // printThird.run() outputs "third". Do not change or remove this line.
+            printThird.run();
+            currNum++;
+            notifyAll();
+        }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
