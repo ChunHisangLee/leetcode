f03abbf9e703d86acfc0aa0471539bad8921c561
jacklee <chunhsiang@mds.com.tw>
2023-06-26 14:21:05 +0800

    2023/06/26 office -2

--- leetcode/editor/en/[356]Line Reflection.java
+++ leetcode/editor/en/[356]Line Reflection.java
@@ -2,41 +2,31 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean isReflected(int[][] points) {
-        Arrays.sort(points, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
-        int sum = points[0][0] + points[points.length - 1][0];
+        Map<Integer, Set<Integer>> map = new HashMap<>();
         for (int[] point : points) {
-            int target = sum - point[0];
-            if (!isCheck(points, target, point[1])) {
-                return false;
+            if (!map.containsKey(point[1])) {
+                map.put(point[1], new HashSet<>());
+            }
+            map.get(point[1]).add(point[0]);
+        }
+        Integer sum = null;
+        for (Set<Integer> set : map.values()) {
+            List<Integer> list = new ArrayList<>(set);
+            Collections.sort(list);
+            int left = 0;
+            int right = list.size() - 1;
+            while (left <= right) {
+                int curr = list.get(left++) + list.get(right--);
+                if (sum == null) {
+                    sum = curr;
+                } else {
+                    if (curr != sum) {
+                        return false;
+                    }
+                }
             }
         }
         return true;
-    }
-
-    public boolean isCheck(int[][] points, int x, int y) {
-        int left = 0;
-        int right = points.length - 1;
-        while (left <= right) {
-            int mid = (left + right) >>> 1;
-            if (points[mid][0] == x && points[mid][1] == y) {
-                return true;
-            }
-            if (points[mid][0] == x) {
-                if (points[mid][1] > y) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            } else {
-                if (points[mid][0] > x) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            }
-
-        }
-        return false;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
