7c4a703f95452bcbb1c074d5bfd906f8e0b4fa3b
ChunHisangLee <goldrushkl168@gmail.com>
2023-06-27 08:56:49 +0800

    2023/06/27 home -1

--- leetcode/editor/en/[373]Find K Pairs with Smallest Sums.java
+++ leetcode/editor/en/[373]Find K Pairs with Smallest Sums.java
@@ -1,19 +1,20 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> (a[0] + a[1]) - (b[0] + b[1]));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[0] + a[1]) - (b[0] + b[1]));
+        List<List<Integer>> res = new ArrayList<>();
         for (int i = 0; i < nums1.length && i < k; i++) {
-            minHeap.add(new int[]{nums1[i], nums2[0], 0});
+            pq.add(new int[]{nums1[i], nums2[0], 1});
         }
-        List<List<Integer>> result = new ArrayList<>();
-        for (int i = 0; i < k && !minHeap.isEmpty(); i++) {
-            int[] curr = minHeap.poll();
-            result.add(List.of(curr[0], curr[1]));
-            int nums2Idx = curr[2];
-            if (nums2Idx < nums2.length - 1)
-                minHeap.add(new int[]{curr[0], nums2[nums2Idx + 1], nums2Idx + 1});
+        while (k-- > 0 && !pq.isEmpty()) {
+            int[] curr = pq.poll();
+            res.add(List.of(curr[0], curr[1]));
+            int index = curr[2];
+            if (index < nums2.length) {
+                pq.add(new int[]{curr[0], nums2[index], index + 1});
+            }
         }
-        return result;
+        return res;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
