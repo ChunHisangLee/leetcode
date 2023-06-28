65b51288662703ba07840d69b752a410badd9892
jacklee <chunhsiang@mds.com.tw>
2023-06-28 18:07:47 +0800

    2023/06/28 office -9

--- leetcode/editor/en/[64]Minimum Path Sum.java
+++ leetcode/editor/en/[64]Minimum Path Sum.java
@@ -4,32 +4,43 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    int[][] dp;
-    int[][] grid;
-
     public int minPathSum(int[][] grid) {
-        this.grid = grid;
         int m = grid.length;
         int n = grid[0].length;
-        dp = new int[m][n];
-        for (int[] arr : dp) {
-            Arrays.fill(arr, -1);
+        int[] dp = new int[n];
+        dp[0] = grid[0][0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = dp[i - 1] + grid[0][i];
         }
-        return dyP(m - 1, n - 1);
-    }
-
-    public int dyP(int i, int j) {
-        if (i == 0 && j == 0) {
-            return grid[i][j];
+        for (int i = 1; i < m; i++) {
+            dp[0] += grid[i][0];
+            for (int j = 1; j < n; j++) {
+                dp[j] = Math.min(dp[j], dp[j - 1]) + grid[i][j];
+            }
         }
-        if (i < 0 || j < 0) {
-            return (int) 4e6;
-        }
-        if (dp[i][1] != -1) {
-            return dp[i][j];
-        }
-        dp[i][j] = Math.min((grid[i][j] + dyP(i - 1, j)), (grid[i][j] + dyP(i, j - 1)));
-        return dp[i][j];
+        return dp[n - 1];
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
+/*
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
+ */
