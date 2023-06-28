99b17e0fbd4ca2004198e1fdf15c430e0ee351de
jacklee <chunhsiang@mds.com.tw>
2023-06-28 17:36:03 +0800

    2023/06/28 office -8

--- leetcode/editor/en/[62]Unique Paths.java
+++ leetcode/editor/en/[62]Unique Paths.java
@@ -6,6 +6,22 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
+        for (int[] arr : dp) {
+            Arrays.fill(arr, 1);
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+/*
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
         dp[0][0]=1;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
@@ -20,4 +36,4 @@ class Solution {
         return dp[m - 1][n - 1];
     }
 }
-//leetcode submit region end(Prohibit modification and deletion)
+ */
