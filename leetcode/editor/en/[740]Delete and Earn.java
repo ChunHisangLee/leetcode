864786a99970a54e597d2745420e040c5506922f
jacklee <chunhsiang@mds.com.tw>
2023-06-28 15:57:16 +0800

    2023/06/28 office -7

--- leetcode/editor/en/[740]Delete and Earn.java
+++ leetcode/editor/en/[740]Delete and Earn.java
@@ -4,29 +4,21 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    Map<Integer, Integer> map = new HashMap<>();
-    Map<Integer, Integer> point = new HashMap<>();
-
     public int deleteAndEarn(int[] nums) {
         int max = 0;
         for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + num);
             max = Math.max(max, num);
         }
-        return dp(max);
-    }
-
-    private int dp(int n) {
-        if (n == 0) {
-            return 0;
+        int[] dp = new int[max + 1];
+        int[] count = new int[max + 1];
+        for (int num : nums) {
+            count[num] += num;
         }
-        if (n == 1) {
-            return map.getOrDefault(n, 0);
+        dp[1] = count[1];
+        for (int i = 2; i <= max; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + count[i]);
         }
-        if (!point.containsKey(n)) {
-            point.put(n, Math.max(dp(n - 1), dp(n - 2) + map.getOrDefault(n, 0)));
-        }
-        return point.get(n);
+        return dp[max];
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
