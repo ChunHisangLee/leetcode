f8ca4dce36915858a0c0822b071c711774230e9f
jacklee <chunhsiang@mds.com.tw>
2023-06-30 10:10:54 +0800

    2023/06/30 office -1

--- leetcode/editor/en/[1970]Last Day Where You Can Still Cross.java
+++ leetcode/editor/en/[1970]Last Day Where You Can Still Cross.java
@@ -1,77 +1,59 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
+
+
 class Solution {
     public int latestDayToCross(int row, int col, int[][] cells) {
-        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        int[][] arr = new int[row][col];
-        for (int[] cell : cells) {
-            grid[cell[0] - 1][cell[1] - 1] = 1;
-        }
         UnionFind uf = new UnionFind(row * col + 2);
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (grid[i][j] == 0) {
-                    for (int[] dir : dirs) {
-                        int x = i + dir[0];
-                        int y = j + dir[1];
-                        if (x < 0 || x >= row || y < 0 || y >= col || grid[x][y] == 1) {
-                            continue;
-                        }
-                        uf.union(i * col + j, x * col + y);
-                    }
-                }
-            }
-        }
-        for (int i = 0; i < col; i++) {
-            uf.union(row * col, i);
-            uf.union(row * col + 1, (row - 1) * col + i);
-        }
-        for (int c = row * col - 1; c >= 0; c--) {
-            int i = cell[c][0] - 1;
-            int j = cell[c][1] - 1;
-            grid[x][y] = 0;
+        int[][] grid = new int[row][col];
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+        for (int i = 0; i < row * col; i++) {
+            int r = cells[i][0] - 1;
+            int c = cells[i][1] - 1;
+            grid[r][c] = 1;
+            int index1 = r * col + c + 1;
             for (int[] dir : dirs) {
-                int x = i + dir[0];
-                int y = j + dir[1];
-                if (x < 0 || x >= row || y < 0 || y >= col || grid[x][y] == 1) {
+                int x = r + dir[0];
+                int y = c + dir[1];
+                int index2 = x * col + y + 1;
+                if (x < 0 || x >= row || y < 0 || y >= col || grid[x][y] == 0) {
                     continue;
                 }
-                uf.union(i * col + j, x * col + y);
+                uf.union(index1, index2);
             }
-            if (uf.isConnected(row * col, row * col + 1)) {
+            if (c == 0) {
+                uf.union(0, index1);
+            }
+            if (c == col - 1) {
+                uf.union(row * col + 1, index1);
+            }
+            if (uf.find(0) == uf.find(row * col + 1)) {
                 return i;
             }
         }
-        return 0;
+        return -1;
     }
 }
-
 class UnionFind {
-    private ing[] group;
+    int[] group;
 
     public UnionFind(int n) {
-        group = new ing[n];
+        group = new int[n];
         for (int i = 0; i < n; i++) {
             group[i] = i;
         }
     }
 
-    public inf find(int x) {
-        return group(x) == x ? x : (group(x) = find(x));
+    public int find(int x) {
+        return group[x] == x ? x : (group[x] = find(group[x]));
     }
 
-    private boolean union(int i, int j) {
+    public void union(int i, int j) {
         int x = find(i);
         int y = find(j);
         if (x != y) {
-            group[x]] =y;
-            return true;
+            group[x] = y;
         }
-        return false;
-    }
-
-    private boolean isConnected(int x, int y) {
-        return find(x) == find(y);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
