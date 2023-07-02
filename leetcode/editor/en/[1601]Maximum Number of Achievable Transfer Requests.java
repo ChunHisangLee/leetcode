144c26eb1b55e2adc50d84ffc217403cc6cffbd2
jacklee <chunhsiang@mds.com.tw>
2023-07-02 09:04:14 +0800

    2023/07/2 home -1

---
+++ leetcode/editor/en/[1601]Maximum Number of Achievable Transfer Requests.java
@@ -0,0 +1,29 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    int res = 0;
+
+    public int maximumRequests(int n, int[][] requests) {
+        backtrack(requests, new int[n], 0, 0);
+        return res;
+    }
+
+    private void backtrack(int[][] requests, int[] inDegree, int index, int count) {
+        if (index == requests.length) {
+            for (int num : inDegree) {
+                if (num != 0) {
+                    return;
+                }
+            }
+            res = Math.max(res, count);
+            return;
+        }
+        inDegree[requests[index][0]]--;
+        inDegree[requests[index][1]]++;
+        backtrack(requests, inDegree, index + 1, count + 1);
+        inDegree[requests[index][0]]++;
+        inDegree[requests[index][1]]--;
+        backtrack(requests, inDegree, index + 1, count);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
