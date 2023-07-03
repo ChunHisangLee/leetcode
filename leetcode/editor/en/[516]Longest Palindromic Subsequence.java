93b98b4d2ff18bd7d6978fc6e3f2606d215908cf
jacklee <chunhsiang@mds.com.tw>
2023-07-03 15:39:16 +0800

    2023/07/3 office -4

--- leetcode/editor/en/[516]Longest Palindromic Subsequence.java
+++ leetcode/editor/en/[516]Longest Palindromic Subsequence.java
@@ -2,38 +2,55 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int longestPalindromeSubseq(String s) {
-
+        char[] c = s.toCharArray();
+        int n = c.length;
+        int[][] dp = new int[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (c[i] == c[j]) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][n - 1];
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 /*
+- 36ms
 class Solution {
+    Integer[][] dp;
+
     public int longestPalindromeSubseq(String s) {
         char[] c = s.toCharArray();
         int n = c.length;
-        int[][] dp = new int[n][n];
-        return dyP(c, 0, n - 1, dp);
+        dp = new Integer[n][n];
+        return dyP(c, 0, n - 1);
     }
 
-    public int dyP(char[] c, int i, int j, int[][] dp) {
+    private int dyP(char[] c, int i, int j) {
         if (i > j) {
             return 0;
         }
         if (i == j) {
             return 1;
         }
-        if (dp[i][j] != 0) {
+        if (dp[i][j] != null) {
             return dp[i][j];
         }
         if (c[i] == c[j]) {
-            dp[i][j] = dyP(c, i + 1, j - 1, dp) + 2;
+            dp[i][j] = dyP(c, i + 1, j - 1) + 2;
         } else {
-            dp[i][j] = Math.max(dyP(c, i + 1, j, dp), dyP(c, i, j - 1, dp));
+            dp[i][j] = Math.max(dyP(c, i + 1, j), dyP(c, i, j - 1));
         }
         return dp[i][j];
     }
 }
 
+- 26ms
 class Solution {
     public int longestPalindromeSubseq(String s) {
         char[] c = s.toCharArray();
@@ -52,5 +69,4 @@ class Solution {
         return dp[0][n - 1];
     }
 }
-
  */
--- leetcode/editor/en/[72]Edit Distance.java
+++ leetcode/editor/en/[72]Edit Distance.java
@@ -4,38 +4,28 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    Integer[][] dp;
-
     public int minDistance(String word1, String word2) {
         char[] c1 = word1.toCharArray();
         char[] c2 = word2.toCharArray();
         int m = c1.length;
         int n = c2.length;
-        dp = new Integer[m + 1][n + 1];
-        return dyP(c1, c2, m, n);
-    }
-
-    private int dyP(char[] c1, char[] c2, int i, int j) {
-        if (i == 0) {
-            return j;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = i;
         }
-        if (j == 0) {
-            return i;
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = j;
         }
-        if (dp[i][j] != null) {
-            return dp[i][j];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (c1[i - 1] == c2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                }
+            }
         }
-        int dist = 0;
-        if (c1[i - 1] == c2[j - 1]) {
-            dist = dyP(c1, c2, i - 1, j - 1);
-        } else {
-            int insertOp = dyP(c1, c2, i, j - 1);
-            int deleteOp = dyP(c1, c2, i - 1, j);
-            int replaceOp = dyP(c1, c2, i - 1, j - 1);
-            dist = Math.min(insertOp, Math.min(deleteOp, replaceOp)) + 1;
-        }
-        dp[i][j] = dist;
-        return dist;
+        return dp[m][n];
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
@@ -72,30 +62,23 @@ class Solution {
             int replaceOp = dyP(c1, c2, i - 1, j - 1);
             dist = Math.min(insertOp, Math.min(deleteOp, replaceOp)) + 1;
         }
-        dp[i][j] = dist;
-        return dist;
+        return dp[i][j] = dist;
     }
 }
 
-Buttom-Up dypnamic programming -4ms
+Buttom-Up dyPnamic programming -4ms
 class Solution {
     public int minDistance(String word1, String word2) {
         char[] c1 = word1.toCharArray();
         char[] c2 = word2.toCharArray();
         int m = c1.length;
         int n = c2.length;
-        if (m == 0) {
-            return n;
-        }
-        if (n == 0) {
-            return m;
-        }
         int[][] dp = new int[m + 1][n + 1];
         for (int i = 1; i <= m; i++) {
             dp[i][0] = i;
         }
-        for (int i = 1; i <= n; i++) {
-            dp[0][i] = i;
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = j;
         }
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
