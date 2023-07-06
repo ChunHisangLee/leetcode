5ec75307216f09e018f6b950595cb4d698dfd6fc
jacklee <chunhsiang@mds.com.tw>
2023-07-06 11:22:26 +0800

    2023/07/6 office -2

--- leetcode/editor/en/[1027]Longest Arithmetic Subsequence.java
+++ leetcode/editor/en/[1027]Longest Arithmetic Subsequence.java
@@ -2,6 +2,30 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int longestArithSeqLength(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int num : nums) {
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+        }
+        int diff = max - min;
+        int n = nums.length;
+        int[][] dp = new int[n][diff * 2 + 1];
+        int res = 2;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = nums[i] - nums[j] + diff;
+                dp[i][temp] = dp[j][temp] == 0 ? 2 : dp[j][temp] + 1;
+                res = Math.max(res, dp[i][temp]);
+            }
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+/*
+class Solution {
+    public int longestArithSeqLength(int[] nums) {
         int res = 2;
         int n = nums.length;
         Map<Integer, Integer>[] dp = new HashMap[n];
@@ -16,4 +40,4 @@ class Solution {
         return res;
     }
 }
-//leetcode submit region end(Prohibit modification and deletion)
+ */
