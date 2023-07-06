fc87ce1aba37ed2496b98e499dd7836a15faf92b
jacklee <chunhsiang@mds.com.tw>
2023-07-06 15:48:30 +0800

    2023/07/6 office -3

---
+++ leetcode/editor/en/[1195]Fizz Buzz Multithreaded.java
@@ -0,0 +1,71 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class FizzBuzz {
+    private int n;
+    private int count = 1;
+
+    public FizzBuzz(int n) {
+        this.n = n;
+    }
+
+    // printFizz.run() outputs "fizz".
+    public void fizz(Runnable printFizz) throws InterruptedException {
+        synchronized (this) {
+            while (count <= n) {
+                if (count % 3 == 0 && count % 5 != 0) {
+                    printFizz.run();
+                    count++;
+                    notifyAll();
+                } else {
+                    wait();
+                }
+            }
+        }
+    }
+
+    // printBuzz.run() outputs "buzz".
+    public void buzz(Runnable printBuzz) throws InterruptedException {
+        synchronized (this) {
+            while (count <= n) {
+                if (count % 5 == 0 && count % 3 != 0) {
+                    printBuzz.run();
+                    count++;
+                    notifyAll();
+                } else {
+                    wait();
+                }
+            }
+        }
+    }
+
+    // printFizzBuzz.run() outputs "fizzbuzz".
+    public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+        synchronized (this) {
+            while (count <= n) {
+                if (count % 15 == 0) {
+                    printFizzBuzz.run();
+                    count++;
+                    notifyAll();
+                } else {
+                    wait();
+                }
+            }
+        }
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void number(IntConsumer printNumber) throws InterruptedException {
+        synchronized (this) {
+            while (count <= n) {
+                if (count % 3 != 0 && count % 5 != 0) {
+                    printNumber.accept(count);
+                    count++;
+                    notifyAll();
+                } else {
+                    wait();
+                }
+            }
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
--- leetcode/editor/en/[646]Maximum Length of Pair Chain.java
+++ leetcode/editor/en/[646]Maximum Length of Pair Chain.java
@@ -2,7 +2,52 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int findLongestChain(int[][] pairs) {
-        
+        Arrays.sort(pairs, (a, b) -> a[1] - b[1]);
+        int curr = Integer.MIN_VALUE;
+        int res = 0;
+        for (int[] pair : pairs) {
+            if (curr < pair[0]) {
+                res++;
+                curr = pair[1];
+            }
+        }
+        return res;
     }
 }
+
+/*
+- 10ms
+class Solution {
+    public int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs, (a, b) -> a[1] - b[1]);
+        int curr = Integer.MIN_VALUE;
+        int res = 0;
+        for (int[] pair : pairs) {
+            if (pair[0] > curr) {
+                res++;
+                curr = pair[1];
+            }
+        }
+        return res;
+    }
+}
+
+- 35ms
+class Solution {
+    public int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        int n = pairs.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (pairs[i][0] > pairs[j][1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return dp[n - 1];
+    }
+}
+ */
 //leetcode submit region end(Prohibit modification and deletion)
