be611a475af71198c7d7f2a9f387f98c0b995a19
jacklee <chunhsiang@mds.com.tw>
2023-07-06 17:23:47 +0800

    2023/07/6 office -4

--- leetcode/editor/en/[1027]Longest Arithmetic Subsequence.java
+++ leetcode/editor/en/[1027]Longest Arithmetic Subsequence.java
@@ -2,21 +2,15 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int longestArithSeqLength(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        for (int num : nums) {
-            max = Math.max(max, num);
-            min = Math.min(min, num);
-        }
-        int diff = max - min;
         int n = nums.length;
-        int[][] dp = new int[n][diff * 2 + 1];
-        int res = 2;
-        for (int i = 1; i < n; i++) {
+        Map<Integer, Integer>[] dp = new HashMap[n];
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            dp[i] = new HashMap<>();
             for (int j = 0; j < i; j++) {
-                int temp = nums[i] - nums[j] + diff;
-                dp[i][temp] = dp[j][temp] == 0 ? 2 : dp[j][temp] + 1;
-                res = Math.max(res, dp[i][temp]);
+                int diff = nums[i] - nums[j];
+                dp[i].put(diff, dp[j].getOrDefault(diff, 1) + 1);
+                res = Math.max(res, dp[i].get(diff));
             }
         }
         return res;
@@ -48,18 +42,18 @@ class Solution {
     }
 }
 
-- 667ms
+- 621ms
 class Solution {
     public int longestArithSeqLength(int[] nums) {
-        int res = 2;
         int n = nums.length;
         Map<Integer, Integer>[] dp = new HashMap[n];
-        for (int right = 0; right < n; right++) {
-            dp[right] = new HashMap<>();
-            for (int left = 0; left < right; left++) {
-                int diff = nums[right] - nums[left];
-                dp[right].put(diff, dp[left].getOrDefault(diff, 1) + 1);
-                res = Math.max(res, dp[right].get(diff));
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            dp[i] = new HashMap<>();
+            for (int j = 0; j < i; j++) {
+                int diff = nums[i] - nums[j];
+                dp[i].put(diff, dp[j].getOrDefault(diff, 1) + 1);
+                res = Math.max(res, dp[i].get(diff));
             }
         }
         return res;
--- leetcode/editor/en/[1218]Longest Arithmetic Subsequence of Given Difference.java
+++ leetcode/editor/en/[1218]Longest Arithmetic Subsequence of Given Difference.java
@@ -2,23 +2,11 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int longestSubsequence(int[] arr, int difference) {
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
+        Map<Integer, Integer> dp = new HashMap<>();
+        int res = 0;
         for (int num : arr) {
-            max = Math.max(max, num);
-            min = Math.min(min, num);
-        }
-        int[] dp = new int[max - min + 1];
-        int res = 1;
-        for (int i = 0; i < arr.length; i++) {
-            int curr = arr[i] - min;
-            int prev = curr - difference;
-            if (prev >= 0 && prev < dp.length) {
-                dp[curr] = dp[prev] + 1;
-            } else {
-                dp[curr] = 1;
-            }
-            res = Math.max(res, dp[curr]);
+            dp.put(num, dp.getOrDefault(num - difference, 0) + 1);
+            res = Math.max(res, dp.get(num));
         }
         return res;
     }
