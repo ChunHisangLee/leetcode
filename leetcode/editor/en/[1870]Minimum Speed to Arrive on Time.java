65d61cb15db5027f5041af15a1fc88c889070db5
jacklee <chunhsiang@mds.com.tw>
2023-07-26 10:03:59 +0800

    2023/07/26 office -1

--- leetcode/editor/en/[1870]Minimum Speed to Arrive on Time.java
+++ leetcode/editor/en/[1870]Minimum Speed to Arrive on Time.java
@@ -8,7 +8,7 @@ class Solution {
         int left = 1;
         int right = (int) 1e7;
         while (left <= right) {
-            int mid = (left + right) >>> 1;
+            int mid = (left + right) >> 1;
             if (isCheck(dist, mid, hour)) {
                 left = mid + 1;
             } else {
