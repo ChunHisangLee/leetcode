d7e71e184bb54c433dbcd5446f778393c223ebc7
ChunHisangLee <goldrushkl168@gmail.com>
2023-08-14 23:56:53 +0800

    2023/08/14 JS home -4

--- leetcode/editor/en/[198]House Robber.js
+++ leetcode/editor/en/[198]House Robber.js
@@ -8,9 +8,7 @@ var rob = function (nums) {
     if (n === 1) {
         return nums[0];
     }
-    let dp = new Array(n);
-    dp[0] = nums[0];
-    dp[1] = Math.max(nums[0], nums[1]);
+    let dp = [nums[0], Math.max(nums[0], nums[1])];
     for (let i = 2; i < n; i++) {
         dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
     }
