6a836de7a6fc85fa9293f1344d9a82c9b00bf69d
jacklee <chunhsiang@mds.com.tw>
2023-08-23 16:54:29 +0800

    2023/08/23 -office -7

--- leetcode/editor/en/[21]Merge Two Sorted Lists.js
+++ leetcode/editor/en/[21]Merge Two Sorted Lists.js
@@ -12,19 +12,19 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function (list1, list2) {
-    let res = new ListNode();
-    let cur = res;
+    let dummy = new ListNode();
+    let curr = dummy;
     while (list1 && list2) {
         if (list1.val < list2.val) {
-            cur.next = list1;
+            curr.next = list1;
             list1 = list1.next;
         } else {
-            cur.next = list2;
+            curr.next = list2;
             list2 = list2.next;
         }
-        cur = cur.next;
+        curr = curr.next;
     }
-    cur.next = list1 || list2;
-    return res.next;
+    curr.next = list1 || list2;
+    return dummy.next;
 };
 //leetcode submit region end(Prohibit modification and deletion)
