15703ec47d9203121624d3bc8559b6eecb4a58a7
jacklee <chunhsiang@mds.com.tw>
2023-08-24 11:13:17 +0800

    2023/08/24 -office -3

---
+++ leetcode/editor/en/[9]Palindrome Number.js
@@ -0,0 +1,18 @@
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+    if (x < 0 || (x % 10 === 10 && x !== 0)) {
+        return false;
+    }
+    let res = 0;
+    let num = x;
+    while (x !== 0) {
+        res = res * 10 + x % 10;
+        x = Math.floor(x / 10);
+    }
+    return num === res;
+};
+//leetcode submit region end(Prohibit modification and deletion)
