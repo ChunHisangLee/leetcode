3401a0173069a1990e73216df4e9e09087953a5e
ChunHisangLee <goldrushkl168@gmail.com>
2023-08-24 22:01:52 +0800

    2023/08/24 home -1

--- leetcode/editor/en/[200]Number of Islands.js
+++ leetcode/editor/en/[200]Number of Islands.js
@@ -1,10 +1,32 @@
-
 //leetcode submit region begin(Prohibit modification and deletion)
 /**
  * @param {character[][]} grid
  * @return {number}
  */
-var numIslands = function(grid) {
-    
+var numIslands = function (grid) {
+    let count = 0;
+    let m = grid.length;
+    let n = grid[0].length;
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === '1') {
+                count++;
+                dfs(grid, i, j, m, n);
+            }
+        }
+    }
+    return count;
 };
+
+function dfs(grid, i, j, m, n) {
+    if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] === '0') {
+        return;
+    }
+    grid[i][j] = '0';
+    dfs(grid, i + 1, j, m, n);
+    dfs(grid, i - 1, j, m, n);
+    dfs(grid, i, j + 1, m, n);
+    dfs(grid, i, j - 1, m, n);
+}
+
 //leetcode submit region end(Prohibit modification and deletion)
