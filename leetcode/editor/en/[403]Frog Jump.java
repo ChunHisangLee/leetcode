7bc59c3028f8fba70d2680ed401ac5dd967891b3
ChunHisangLee <goldrushkl168@gmail.com>
2023-08-27 14:36:55 +0800

    2023/08/27 home -1

--- leetcode/editor/en/[403]Frog Jump.java
+++ leetcode/editor/en/[403]Frog Jump.java
@@ -1,10 +1,93 @@
 403
-Frog Jump
-2022-12-26 11:04:53
+        Frog Jump
+        2022-12-26 11:04:53
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean canCross(int[] stones) {
-        
+        if (stones[1] != 1) {
+            return false;
+        }
+        Map<Integer, Set<Integer>> dp = new HashMap<>();
+        for (int stone : stones) {
+            dp.put(stone, new HashSet<>());
+        }
+        dp.get(stones[0]).add(1);
+        for (int stone : stones) {
+            for (int jump : dp.get(stone)) {
+                if (jump != 0 && dp.containsKey(stone + jump)) {
+                    dp.get(stone + jump).add(jump - 1);
+                    dp.get(stone + jump).add(jump);
+                    dp.get(stone + jump).add(jump + 1);
+                }
+            }
+        }
+        return !dp.get(stones[stones.length - 1]).isEmpty();
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
+/*
+Top - Down - 10 ms
+class Solution {
+    Boolean[][] dp;
+    int n;
+
+    public boolean canCross(int[] stones) {
+        n = stones.length;
+        dp = new Boolean[n + 1][n + 1];
+        return dyP(stones, 0, 0);
+    }
+
+    private boolean dyP(int[] stones, int index, int prev) {
+        if (index == n - 1) {
+            return true;
+        }
+        if (dp[index][prev] != null) {
+            return dp[index][prev];
+        }
+        int max = stones[index] + prev + 1;
+        for (int next = index + 1; next < n && stones[next] <= max; next++) {
+            if (stones[next] == stones[index] + prev - 1) {
+                if (dyP(stones, next, prev - 1)) {
+                    return true;
+                }
+            }
+            if (stones[next] == stones[index] + prev) {
+                if (dyP(stones, next, prev)) {
+                    return true;
+                }
+            }
+            if (stones[next] == stones[index] + prev + 1) {
+                if (dyP(stones, next, prev + 1)) {
+                    return true;
+                }
+            }
+        }
+        return dp[index][prev] = false;
+    }
+}
+
+- 57 ms
+class Solution {
+    public boolean canCross(int[] stones) {
+        if (stones[1] != 1) {
+            return false;
+        }
+        Map<Integer, Set<Integer>> dp = new HashMap<>();
+        for (int stone : stones) {
+            dp.put(stone, new HashSet<>());
+        }
+        dp.get(stones[0]).add(1);
+        for (int stone : stones) {
+            for (int jump : dp.get(stone)) {
+                if (jump != 0 && dp.containsKey(stone + jump)) {
+                    dp.get(stone + jump).add(jump - 1);
+                    dp.get(stone + jump).add(jump);
+                    dp.get(stone + jump).add(jump + 1);
+                }
+            }
+        }
+        return !dp.get(stones[stones.length - 1]).isEmpty();
+    }
+}
+ */
---
+++ leetcode/editor/en/[403]Frog Jump.js
@@ -0,0 +1,31 @@
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[]} stones
+ * @return {boolean}
+ */
+var canCross = function (stones) {
+    
+};
+//leetcode submit region end(Prohibit modification and deletion)
+/*
+var canCross = function (stones) {
+    if (stones[1] !== 1) {
+        return false;
+    }
+    let dp = new Map();
+    for (let stone of stones) {
+        dp.set(stone, new Set());
+    }
+    dp.get(stones[0]).add(1);
+    for (let stone of stones) {
+        for (let jump of dp.get(stone)) {
+            if (jump !== 0 && dp.has(stone + jump)) {
+                dp.get(stone + jump).add(jump);
+                dp.get(stone + jump).add(jump + 1);
+                dp.get(stone + jump).add(jump - 1);
+            }
+        }
+    }
+    return dp.get(stones[stones.length - 1]).size > 0;
+};
+ */
