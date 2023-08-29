b5efcc6cc7be0c95b16c2dd31137bf017fca6483
jacklee <chunhsiang@mds.com.tw>
2023-08-29 11:09:17 +0800

    2023/08/25 -office -3

--- leetcode/editor/en/[1183]Maximum Number of Ones.js
+++ leetcode/editor/en/[1183]Maximum Number of Ones.js
@@ -1,4 +1,3 @@
-
 //leetcode submit region begin(Prohibit modification and deletion)
 /**
  * @param {number} width
@@ -7,7 +6,20 @@
  * @param {number} maxOnes
  * @return {number}
  */
-var maximumNumberOfOnes = function(width, height, sideLength, maxOnes) {
-    
+var maximumNumberOfOnes = function (width, height, sideLength, maxOnes) {
+    let arr = new Array(sideLength * sideLength).fill(0);
+    for (let i = 0; i < height; i++) {
+        let row = i % sideLength;
+        for (let j = 0; j < width; j++) {
+            let col = j % sideLength;
+            arr[(row * sideLength) + col]++;
+        }
+    }
+    arr.sort((a, b) => b - a);
+    let res = 0;
+    for (let i = 0; i < maxOnes; i++) {
+        res += arr[i];
+    }
+    return res;
 };
 //leetcode submit region end(Prohibit modification and deletion)
