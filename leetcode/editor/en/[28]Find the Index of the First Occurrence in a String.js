d25f4b8f3005e64188c1e609107bdbd900af34ab
jacklee <chunhsiang@mds.com.tw>
2023-09-05 14:32:25 +0800

    2023/09/05 -office -3

---
+++ leetcode/editor/en/[28]Find the Index of the First Occurrence in a String.js
@@ -0,0 +1,11 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle);
+};
+//leetcode submit region end(Prohibit modification and deletion)
