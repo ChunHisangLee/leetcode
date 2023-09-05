547766aa9efd0e7ad1fc6a2cf004c7e3d7b97166
jacklee <chunhsiang@mds.com.tw>
2023-09-05 10:12:46 +0800

    2023/09/05 -office -2

--- leetcode/editor/en/[138]Copy List with Random Pointer.js
+++ leetcode/editor/en/[138]Copy List with Random Pointer.js
@@ -84,14 +84,12 @@ var copyRandomList = function (head) {
         map.set(node, new Node(node.val));
         node = node.next;
     }
-
     node = head;
     while (node) {
         map.get(node).next = map.get(node.next) || null;
         map.get(node).random = map.get(node.random) || null;
         node = node.next;
     }
-
     return map.get(head);
 };
 //leetcode submit region end(Prohibit modification and deletion)
