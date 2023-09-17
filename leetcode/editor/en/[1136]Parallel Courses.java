6c7f9c962a95fc8dad9c87a5786648eadfc64dcb
Jack Lee <goldrushkl168@gmail.com>
2023-09-17 09:34:13 +0800

    2023/09/16 home -1

--- leetcode/editor/en/[1136]Parallel Courses.java
+++ leetcode/editor/en/[1136]Parallel Courses.java
@@ -5,33 +5,42 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int minimumSemesters(int n, int[][] relations) {
-        List<List<Integer>> list = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            list.add(new ArrayList<>());
-        }
+        List<List<Integer>> graph = new ArrayList<>();
         int[] inDegree = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
         for (int[] relation : relations) {
             int prev = relation[0];
             int next = relation[1];
-            list.get(prev).add(next);
+            graph.get(prev).add(next);
             inDegree[next]++;
         }
+
         int count = 0;
         Deque<Integer> dq = new ArrayDeque<>();
-        for (int i = 0; i <= n; i++) {
+
+        for (int i = 1; i <= n; i++) {
             if (inDegree[i] == 0) {
                 dq.add(i);
                 count++;
             }
         }
+
         int res = 0;
+
         while (!dq.isEmpty()) {
             int size = dq.size();
             res++;
+
             for (int i = 0; i < size; i++) {
                 int index = dq.poll();
-                for (int num : list.get(index)) {
+
+                for (int num : graph.get(index)) {
                     inDegree[num]--;
+
                     if (inDegree[num] == 0) {
                         dq.add(num);
                         count++;
@@ -39,7 +48,8 @@ class Solution {
                 }
             }
         }
-        return n == count - 1 ? res : -1;
+
+        return n == count ? res : -1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
--- leetcode/editor/en/[1584]Min Cost to Connect All Points.js
+++ leetcode/editor/en/[1584]Min Cost to Connect All Points.js
@@ -82,21 +82,26 @@ var minCostConnectPoints = function (points) {
     const n = points.length;
     let res = 0;
     let count = 0;
+
     let isVisited = new Array(n).fill(false);
     let arr = new Array(n).fill(Infinity);
     arr[0] = 0;
+
     while (count < n) {
         let num = Infinity;
         let vertex = -1;
+
         for (let i = 0; i < n; i++) {
             if (!isVisited[i] && num > arr[i]) {
                 num = arr[i];
                 vertex = i;
             }
         }
+
         res += num;
         count++;
         isVisited[vertex] = true;
+
         for (let i = 0; i < n; i++) {
             let weight = Math.abs(points[vertex][0] - points[i][0]) +
                 Math.abs(points[vertex][1] - points[i][1]);
@@ -105,6 +110,7 @@ var minCostConnectPoints = function (points) {
             }
         }
     }
+    
     return res;
 };
  */
