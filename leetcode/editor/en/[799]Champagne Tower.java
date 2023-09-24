b503b4b1fbdb3f0e1df38c317446defcfa2f9e36
Jack Lee <goldrushkl168@gmail.com>
2023-09-24 16:11:21 +0800

    2023/09/24 home -1

--- leetcode/editor/en/[799]Champagne Tower.java
+++ leetcode/editor/en/[799]Champagne Tower.java
@@ -2,22 +2,25 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public double champagneTower(int poured, int query_row, int query_glass) {
-        double[][] arrs = new double[query_row + 1][query_row + 1];
+        double[] arr = new double[query_row + 1];
 
-        arrs[0][0] = (double) poured;
+        arr[0] = (double) poured;
 
         for (int i = 0; i < query_row; i++) {
+            double[] nextArr = new double[query_row + 1];
+
             for (int j = 0; j <= i; j++) {
-                double temp = (arrs[i][j] - 1.0) / 2.0;
+                double temp = (arr[j] - 1.0) / 2.0;
 
                 if (temp > 0) {
-                    arrs[i + 1][j] += temp;
-                    arrs[i + 1][j + 1] += temp;
+                    nextArr[j] += temp;
+                    nextArr[j + 1] += temp;
                 }
             }
+            arr = nextArr;
         }
 
-        return Math.min(1, arrs[query_row][query_glass]);
+        return Math.min(1, arr[query_glass]);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
--- leetcode/editor/en/[799]Champagne Tower.js
+++ leetcode/editor/en/[799]Champagne Tower.js
@@ -6,21 +6,20 @@
  * @return {number}
  */
 var champagneTower = function (poured, query_row, query_glass) {
-    let arrs = Array.from({length: query_row + 1}, () => new Array(query_row + 1).fill(0));
-
-    arrs[0][0] = poured;
+    let arr = [poured];
 
     for (let i = 0; i < query_row; i++) {
-        for (let j = 0; j <= i; j++) {
-            let temp = (arrs[i][j] - 1.0) / 2.0;
+        let nextArr = new Array(query_row + 1).fill(0);
 
+        for (let j = 0; j <= i; j++) {
+            let temp = (arr[j] - 1) / 2;
             if (temp > 0) {
-                arrs[i + 1][j] += temp;
-                arrs[i + 1][j + 1] += temp;
+                nextArr[j] += temp;
+                nextArr[j + 1] += temp;
             }
         }
+        arr = nextArr
     }
-
-    return Math.min(1, arrs[query_row][query_glass]);
+    return Math.min(1, arr[query_glass])
 };
 //leetcode submit region end(Prohibit modification and deletion)
