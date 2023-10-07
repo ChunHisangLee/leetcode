fa0da39a7063068ce459000d2fc1ef22da306bdc
Jack Lee <goldrushkl168@gmail.com>
2023-10-07 08:45:36 +0800

    2023/10/07 home -1

---
+++ leetcode/editor/en/[1420]Build Array Where You Can Find The Maximum Exactly K Comparisons.java
@@ -0,0 +1,36 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int numOfArrays(int n, int m, int k) {
+        long[][][] dp = new long[n][k][m];
+        final int MOD = (int)1e9+7;
+        long result = 0;
+
+        Arrays.fill(dp[0][0], 1);
+
+        for (int i = 1; i < n; i++) {
+            for (int cost = 0; cost < Math.min(i + 1, k); cost++) {
+                for (int max = 0; max < m; max++) {
+                    long sum = 0;
+                    sum += dp[i - 1][cost][max] * (max + 1);
+
+                    if (cost != 0) {
+                        long[] arr = dp[i - 1][cost - 1];
+                        for (int prevMax = 0; prevMax < max; prevMax++) {
+                            sum += arr[prevMax];
+                        }
+                    }
+
+                    dp[i][cost][max] = sum % MOD;
+                }
+            }
+        }
+
+        for (int max = 0; max < m; max++) {
+            result += dp[n - 1][k - 1][max];
+            result %= MOD;
+        }
+        return (int) result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
