f1572b7621986bf0d02fb483454a6baffb7c15b6
Jack Lee <goldrushkl168@gmail.com>
2023-10-19 08:50:09 +0800

    2023/10/19 home -1

--- leetcode/editor/en/[844]Backspace String Compare.java
+++ leetcode/editor/en/[844]Backspace String Compare.java
@@ -5,18 +5,51 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        return build(s).equals(build(t));
+        s = build(s);
+        t = build(t);
+
+        return s.equals(t);
     }
 
     public String build(String s) {
-        Stack<Character> ans = new Stack();
-        for (char c : s.toCharArray()) {
-            if (c != '#')
-                ans.push(c);
-            else if (!ans.empty())
-                ans.pop();
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '#') {
+                count++;
+            } else if (count != 0) {
+                count--;
+            } else {
+                sb.append(s.charAt(i));
+            }
         }
-        return String.valueOf(ans);
+
+        return sb.toString();
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
+/*
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        s = build(s);
+        t = build(t);
+
+        return s.equals(t);
+    }
+
+    public String build(String s) {
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : s.toCharArray()) {
+            if (c != '#') {
+                deque.push(c);
+            } else if (!deque.isEmpty()) {
+                deque.pop();
+            }
+        }
+
+        return String.valueOf(deque);
+    }
+}
+ */
