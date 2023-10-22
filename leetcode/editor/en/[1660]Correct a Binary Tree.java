f4fc03c9551a5d86538bffd5783a104f7aed979b
Jack Lee <goldrushkl168@gmail.com>
2023-10-22 09:33:36 +0800

    2023/10/22 home -3

--- leetcode/editor/en/[1660]Correct a Binary Tree.java
+++ leetcode/editor/en/[1660]Correct a Binary Tree.java
@@ -75,4 +75,24 @@ class Solution {
         return root;
     }
 }
+
+class Solution {
+    Set<Integer> visited = new HashSet<>();
+
+    public TreeNode correctBinaryTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.right != null && visited.contains(root.right.val)) {
+            return null;
+        }
+
+        visited.add(root.val);
+        root.right = correctBinaryTree(root.right);
+        root.left = correctBinaryTree(root.left);
+
+        return root;
+    }
+}
  */
