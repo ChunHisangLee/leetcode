c67d09a3020d2855d83097a122a5823047922a50
Jack Lee <goldrushkl168@gmail.com>
2023-10-28 08:38:51 +0800

    2023/10/28 home -1

--- leetcode/editor/en/[1220]Count Vowels Permutation.java
+++ leetcode/editor/en/[1220]Count Vowels Permutation.java
@@ -5,27 +5,20 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int countVowelPermutation(int n) {
-        int MOD = (int) 1e9 + 7;
-        long aCount = 1;
-        long eCount = 1;
-        long iCount = 1;
-        long oCount = 1;
-        long uCount = 1;
+        final int MOD = (int) 1e9 + 7;
+        long aCount = 1, eCount = 1, iCount = 1, oCount = 1, uCount = 1;
+
         for (int i = 1; i < n; i++) {
-            long aNew = (eCount + iCount + uCount) % MOD;
-            long eNew = (aCount + iCount) % MOD;
-            long iNew = (eCount + oCount) % MOD;
-            long oNew = iCount % MOD;
-            long uNew = (iCount + oCount) % MOD;
-            aCount = aNew;
-            eCount = eNew;
-            iCount = iNew;
-            oCount = oNew;
-            uCount = uNew;
+            long prevA = aCount, prevE = eCount, prevI = iCount, prevO = oCount, prevU = uCount;
+
+            aCount = (prevE + prevI + prevU) % MOD;
+            eCount = (prevA + prevI) % MOD;
+            iCount = (prevE + prevO) % MOD;
+            oCount = prevI % MOD;
+            uCount = (prevI + prevO) % MOD;
         }
-        long res = 0;
-        res = (aCount + eCount + iCount + oCount + uCount) % MOD;
-        return (int) res;
+
+        return (int) ((aCount + eCount + iCount + oCount + uCount) % MOD);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
