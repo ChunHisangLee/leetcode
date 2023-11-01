d176799cf225d36bdaf6523867059d1c2ff6d544
Jack Lee <goldrushkl168@gmail.com>
2023-11-01 21:14:56 +0800

    2023/11/01 1188

--- leetcode/editor/en/[1188]Design Bounded Blocking Queue.java
+++ leetcode/editor/en/[1188]Design Bounded Blocking Queue.java
@@ -1,21 +1,59 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class BoundedBlockingQueue {
+    private Deque<Integer> deque;
+    private int capacity;
+    private ReentrantLock lock;
+    private Condition notEmpty;
+    private Condition notFull;
 
     public BoundedBlockingQueue(int capacity) {
-        
+        this.deque = new ArrayDeque<>();
+        this.capacity = capacity;
+        this.lock = new ReentrantLock();
+        this.notEmpty = lock.newCondition();
+        this.notFull = lock.newCondition();
     }
-    
+
     public void enqueue(int element) throws InterruptedException {
-        
+        lock.lock();
+
+        try {
+            while (deque.size() == capacity) {
+                notFull.await();
+            }
+
+            deque.offer(element);
+            notEmpty.signal();
+        } finally {
+            lock.unlock();
+        }
     }
-    
+
     public int dequeue() throws InterruptedException {
-        
+        lock.lock();
+
+        try {
+            while (deque.isEmpty()) {
+                notEmpty.await();
+            }
+
+            int item = deque.poll();
+            notFull.signal();
+            return item;
+        } finally {
+            lock.unlock();
+        }
     }
-    
+
     public int size() {
-        
+        lock.lock();
+
+        try {
+            return deque.size();
+        } finally {
+            lock.unlock();
+        }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
