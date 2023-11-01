c4a241ffd60db1f178c781dfeb8823988fa612e5
Jack Lee <goldrushkl168@gmail.com>
2023-11-01 21:01:44 +0800

    2023/11/01 1226

---
+++ leetcode/editor/en/[1226]The Dining Philosophers.java
@@ -0,0 +1,66 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+class DiningPhilosophers {
+    public DiningPhilosophers() {
+
+    }
+
+    // call the run() method of any runnable to execute its code
+    public void wantsToEat(int philosopher,
+                           Runnable pickLeftFork,
+                           Runnable pickRightFork,
+                           Runnable eat,
+                           Runnable putLeftFork,
+                           Runnable putRightFork) throws InterruptedException {
+
+        synchronized (this) {
+            pickLeftFork.run();
+            pickRightFork.run();
+            eat.run();
+            putLeftFork.run();
+            putRightFork.run();
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+/*
+class DiningPhilosophers {
+
+    private Semaphore[] forks = new Semaphore[5];
+    private Lock eaters;
+
+    public DiningPhilosophers() {
+        for (int i = 0; i < 5; i++) {
+            forks[i] = new Semaphore(1);
+        }
+
+        eaters = new ReentrantLock();
+    }
+
+    // call the run() method of any runnable to execute its code
+    public void wantsToEat(int philosopher,
+                           Runnable pickLeftFork,
+                           Runnable pickRightFork,
+                           Runnable eat,
+                           Runnable putLeftFork,
+                           Runnable putRightFork) throws InterruptedException {
+
+        eaters.lock();
+        forks[philosopher].acquire();
+        forks[(philosopher + 1) % 5].acquire();
+
+        pickLeftFork.run();
+        pickRightFork.run();
+
+        eat.run();
+
+        putLeftFork.run();
+        putRightFork.run();
+
+        forks[philosopher].release();
+        forks[(philosopher + 1) % 5].release();
+        eaters.unlock();
+    }
+}
+ */
