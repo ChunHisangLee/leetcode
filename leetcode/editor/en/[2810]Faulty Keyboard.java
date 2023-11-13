a35ae37ca27f7a901448bb5b2a5c082742a94810
Jack Lee <goldrushkl168@gmail.com>
2023-11-13 09:23:21 +0800

    2023/11/13 2810

---
+++ leetcode/editor/en/[2810]Faulty Keyboard.java
@@ -0,0 +1,18 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String finalString(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (c == 'i') {
+                sb.reverse();
+            } else {
+                sb.append(c);
+            }
+        }
+
+        return String.valueOf(sb);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
