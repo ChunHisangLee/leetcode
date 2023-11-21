e5632653d641dd93664585e02f56d308fd71e0a4
Jack Lee <goldrushkl168@gmail.com>
2023-11-21 09:08:56 +0800

    2023/11/21  1814

--- leetcode/editor/en/[1814]Count Nice Pairs in an Array.java
+++ leetcode/editor/en/[1814]Count Nice Pairs in an Array.java
@@ -22,8 +22,8 @@ class Solution {
     private int rev(int x) {
         int result = 0;
 
-        while (x != 0) {
-            result = result * 10 + x % 10;
+        while (x > 0) {
+            result = result * 10 + (x % 10);
             x /= 10;
         }
 
