d98a63f952340d3cc7b59b0ea3385960cdeef6aa
Jack Lee <goldrushkl168@gmail.com>
2023-11-23 11:42:54 +0800

    2023/11/23  1630

--- leetcode/editor/en/[1630]Arithmetic Subarrays.java
+++ leetcode/editor/en/[1630]Arithmetic Subarrays.java
@@ -19,6 +19,7 @@ class Solution {
 
         Arrays.sort(arr);
         int diff = arr[1] - arr[0];
+
         for (int i = 2; i < arr.length; i++) {
             if (arr[i] - arr[i - 1] != diff) {
                 return false;
