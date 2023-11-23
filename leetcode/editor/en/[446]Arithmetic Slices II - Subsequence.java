bc50dbeb8b891b959707b6fb86fe8bb3be80b6db
Jack Lee <goldrushkl168@gmail.com>
2023-11-23 09:22:02 +0800

    2023/11/23  446

--- leetcode/editor/en/[446]Arithmetic Slices II - Subsequence.java
+++ leetcode/editor/en/[446]Arithmetic Slices II - Subsequence.java
@@ -6,25 +6,22 @@
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
         int n = nums.length;
-        int ans = 0;
-        int[][] dp = new int[n][n];
-        Map<Long, List<Integer>> numToIndices = new HashMap<>();
-        for (int i = 0; i < n; ++i) {
-            numToIndices.putIfAbsent((long) nums[i], new ArrayList<>());
-            numToIndices.get((long) nums[i]).add(i);
-        }
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                long target = nums[j] * 2L - nums[i];
-                if (numToIndices.containsKey(target)) {
-                    for (final int k : numToIndices.get(target))
-                        if (k < j)
-                            dp[i][j] += (dp[j][k] + 1);
-                }
-                ans += dp[i][j];
+        int count = 0;
+        Map<Long, Integer>[] maps = new Map[n];
+
+        for (int i = 0; i < n; i++) {
+            maps[i] = new HashMap<>();
+
+            for (int j = 0; j < i; j++) {
+                long diff = (long) nums[i] - nums[j];
+                int endAtJ = maps[j].getOrDefault(diff, 0);
+                int endAtI = maps[i].getOrDefault(diff, 0);
+                count += endAtJ;
+                maps[i].put(diff, endAtI + endAtJ + 1);
             }
         }
-        return ans;
+
+        return count;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
