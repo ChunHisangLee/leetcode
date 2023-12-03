5bbe0a483fe208a4746f4943873bc4b822186015
Jack Lee <goldrushkl168@gmail.com>
2023-12-03 09:02:21 +0800

    2023/12/03 408

---
+++ leetcode/editor/en/[408]Valid Word Abbreviation.java
@@ -0,0 +1,41 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean validWordAbbreviation(String word, String abbr) {
+        char[] cWord = word.toCharArray();
+        char[] cAbbr = abbr.toCharArray();
+        int i = 0;
+        int j = 0;
+
+        while (i < cWord.length && j < cAbbr.length) {
+            if (Character.isDigit(cAbbr[j])) {
+                if (cAbbr[j] == '0') {
+                    return false;
+                }
+
+                int num = 0;
+
+                while (j < cAbbr.length && Character.isDigit(cAbbr[j])) {
+                    num = num * 10 + (cAbbr[j] - '0');
+                    j++;
+                }
+
+                i += num;
+            } else {
+                if (i >= cWord.length) {
+                    return false;
+                }
+
+                if (cWord[i] != cAbbr[j]) {
+                    return false;
+                }
+
+                i++;
+                j++;
+            }
+        }
+
+        return i == cWord.length && j == cAbbr.length;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
