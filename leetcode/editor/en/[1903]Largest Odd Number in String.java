581c177e43a26d56f0694d09e45b76f5cf404db4
Jack Lee <goldrushkl168@gmail.com>
2023-12-07 22:20:36 +0800

    2023/12/07 1903

---
+++ leetcode/editor/en/[1903]Largest Odd Number in String.java
@@ -0,0 +1,13 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String largestOddNumber(String num) {
+        for (int i = num.length() - 1; i >= 0; i--) {
+            if ((num.charAt(i) - '0') % 2 != 0) {
+                return num.substring(0, i + 1);
+            }
+        }
+        return "";
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
