e55ad1bd5cc9bd6ca3221a79276e007028011629
ChunHisangLee <goldrushkl168@gmail.com>
2023-12-11 21:18:04 +0800

    2023/12/11 1287

--- leetcode/editor/en/[1287]Element Appearing More Than 25% In Sorted Array.java
+++ leetcode/editor/en/[1287]Element Appearing More Than 25% In Sorted Array.java
@@ -3,19 +3,19 @@
 class Solution {
     public int findSpecialInteger(int[] arr) {
         int bar = arr.length / 4;
-        int result = arr[0];
         int count = 1;
+        int result = arr[0];
 
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] == result) {
                 count++;
-            } else {
-                result = arr[i];
-                count = 1;
-            }
 
-            if (count > bar) {
-                return arr[i];
+                if (count > bar) {
+                    return result;
+                }
+            } else {
+                count = 1;
+                result = arr[i];
             }
         }
 
