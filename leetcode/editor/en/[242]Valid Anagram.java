b00104c0d43f2465f86b860cd494f82ce12a9b16
Jack Lee <goldrushkl168@gmail.com>
2023-12-16 09:46:58 +0800

    2023/12/16 242

--- leetcode/editor/en/[242]Valid Anagram.java
+++ leetcode/editor/en/[242]Valid Anagram.java
@@ -1,22 +1,30 @@
 242
-        Valid Anagram
+Valid Anagram
         2023-01-01 22:43:23
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean isAnagram(String s, String t) {
         int[] arr = new int[26];
-        for (char c : s.toCharArray()) {
-            arr[c - 'a']++;
+        countLetters(s, arr, 1);
+        countLetters(t, arr, -1);
+
+        return checkEqual(arr);
+    }
+
+    private void countLetters(String str, int[] arr, int i) {
+        for (char c : str.toCharArray()) {
+            arr[c - 'a'] += i;
         }
-        for (char c : t.toCharArray()) {
-            arr[c - 'a']--;
-        }
+    }
+
+    private boolean checkEqual(int[] arr) {
         for (int num : arr) {
             if (num != 0) {
                 return false;
             }
         }
+
         return true;
     }
 }
