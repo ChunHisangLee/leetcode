1956964d223c981d7aac649d1ff08e4ff3ec3707
ChunHisangLee <goldrushkl168@gmail.com>
2023-12-20 13:36:44 +0800

    2023/12/20 9, 14

--- editor/en/[14]Longest Common Prefix.java
+++ editor/en/[14]Longest Common Prefix.java
@@ -1,17 +1,20 @@
 14
-        Longest Common Prefix
+Longest Common
+Prefix
         2023-01-02 10:39:06
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        String str = strs[0];
-        for (int i = 1; i < strs.length; i++) {
-            while (strs[i].indexOf(str) != 0) {
-                str = str.substring(0, str.length() - 1);
+        String prefixString = strs[0];
+
+        for (String str : strs) {
+            while (str.indexOf(prefixString) != 0) {
+                prefixString = prefixString.substring(0, prefixString.length() - 1);
             }
         }
-        return str;
+
+        return prefixString;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
--- editor/en/[9]Palindrome Number.java
+++ editor/en/[9]Palindrome Number.java
@@ -1,18 +1,26 @@
 9
 Palindrome Number
 2023-01-02 00:21:24
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0)){
-            return false;}
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int res = getReverse(x);
+        return x == res;
+    }
+
+    private int getReverse(int x) {
         int res = 0;
-        int num = x;
+
         while (x != 0) {
             res = res * 10 + x % 10;
             x = x / 10;
         }
-        return num == res;
+        return res;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
