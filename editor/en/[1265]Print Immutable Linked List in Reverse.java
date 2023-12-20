b0f7120f6f4acfe1fd8d8efdb7de75ac214ecd73
ChunHisangLee <goldrushkl168@gmail.com>
2023-12-20 09:22:58 +0800

    2023/12/20 rearrange the directory

--- editor/en/[1265]Print Immutable Linked List in Reverse.java
+++ editor/en/[1265]Print Immutable Linked List in Reverse.java
@@ -1,14 +1,17 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * // This is the ImmutableListNode's API interface.
+ * // You should not implement it, or speculate about its implementation.
+ * interface ImmutableListNode {
+ *     public void printValue(); // print the value of this node.
+ *     public ImmutableListNode getNext(); // return the next node.
+ * };
+ */
 
 class Solution {
     public void printLinkedListInReverse(ImmutableListNode head) {
-        if (head == null) {
-            return;
-        }
-
-        printLinkedListInReverse(head.getNext());
-        head.printValue();
+        
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
--- editor/en/[206]Reverse Linked List.java
+++ editor/en/[206]Reverse Linked List.java
@@ -1,5 +1,18 @@
+206
+        Reverse Linked List
+        2022-11-29 15:29:54
 //leetcode submit region begin(Prohibit modification and deletion)
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode reverseList(ListNode head) {
         if (head == null) {
