54096cab648abd5dd987e3a7cee4381a7dbe47b3
ChunHisangLee <goldrushkl168@gmail.com>
2023-12-20 13:23:24 +0800

    2023/12/20 88, 121

--- editor/en/[121]Best Time to Buy and Sell Stock.java
+++ editor/en/[121]Best Time to Buy and Sell Stock.java
@@ -7,10 +7,12 @@ class Solution {
     public int maxProfit(int[] prices) {
         int cost = Integer.MAX_VALUE;
         int profit = 0;
+
         for (int price : prices) {
             cost = Math.min(cost, price);
             profit = Math.max(profit, price - cost);
         }
+
         return profit;
     }
 }
--- editor/en/[88]Merge Sorted Array.java
+++ editor/en/[88]Merge Sorted Array.java
@@ -1,14 +1,26 @@
 88
-        Merge Sorted Array
+Merge Sorted
+Array
         2023-01-17 22:18:30
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for (int i = m, j = 0; j < n; i++, j++) {
-            nums1[i] = nums2[j];
+        int last = m + n - 1;
+        int first = m - 1;
+        int second = n - 1;
+
+        while (first >= 0 && second >= 0) {
+            if (nums1[first] > nums2[second]) {
+                nums1[last--] = nums1[first--];
+            } else {
+                nums1[last--] = nums2[second--];
+            }
         }
-        Arrays.sort(nums1);
+
+        while (second >= 0) {
+            nums1[last--] = nums2[second--];
+        }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
