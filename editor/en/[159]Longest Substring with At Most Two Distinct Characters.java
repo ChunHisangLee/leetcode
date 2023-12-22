3638c1cb54629c93ac618d4a28fb8325c5b404fb
ChunHisangLee <goldrushkl168@gmail.com>
2023-12-22 10:35:42 +0800

    2023/12/22 159

--- editor/en/[159]Longest Substring with At Most Two Distinct Characters.java
+++ editor/en/[159]Longest Substring with At Most Two Distinct Characters.java
@@ -1,10 +1,42 @@
 159
-Longest Substring with At Most Two Distinct Characters
+Longest Substring
+with At
+Most Two
+Distinct Characters
 2023-01-31 10:50:45
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int lengthOfLongestSubstringTwoDistinct(String s) {
-        
+        int[] arr = new int[256];
+        int left = 0;
+        int count = 0;
+        int max = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char rightChar = s.charAt(right);
+
+            if (arr[rightChar] == 0) {
+                count++;
+            }
+
+            arr[rightChar]++;
+
+            while (count > 2) {
+                char leftChar = s.charAt(left);
+                arr[leftChar]--;
+
+                if (arr[leftChar] == 0) {
+                    count--;
+                }
+
+                left++;
+            }
+
+            max = Math.max(max, right - left + 1);
+        }
+
+        return max;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
