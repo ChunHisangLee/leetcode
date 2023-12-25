760b77472dff9fc77049cbb2e4116ded8040bc69
Jack Lee <goldrushkl168@gmail.com>
2023-12-25 20:29:45 +0800

    2023/12/25 2957

---
+++ leetcode/editor/en/[2957]Remove Adjacent Almost-Equal Characters.java
@@ -0,0 +1,20 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int removeAlmostEqualCharacters(String word) {
+        int count = 0;
+
+        for (int i = 0; i < word.length() - 1; i++) {
+            char curr = word.charAt(i);
+            char next = word.charAt(i + 1);
+
+            if (curr == next || Math.abs(curr - next) == 1) {
+                count++;
+                i++;
+            }
+        }
+
+        return count;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
