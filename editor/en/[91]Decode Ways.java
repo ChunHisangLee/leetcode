7e4b35f6c2b5f4920e2c7eb09e507b92b76e9f58
ChunHisangLee <goldrushkl168@gmail.com>
2023-12-25 08:59:17 +0800

    2023/12/25 91

--- editor/en/[91]Decode Ways.java
+++ editor/en/[91]Decode Ways.java
@@ -1,5 +1,5 @@
 91
-        Decode Ways
+Decode Ways
         2023-02-23 18:17:12
 
 //leetcode submit region begin(Prohibit modification and deletion)
@@ -9,16 +9,20 @@ class Solution {
         int[] dp = new int[n + 1];
         dp[0] = 1;
         dp[1] = s.charAt(0) == '0' ? 0 : 1;
+
         for (int i = 2; i <= n; i++) {
             char c1 = s.charAt(i - 1);
             char c2 = s.charAt(i - 2);
+
             if (c1 != '0') {
                 dp[i] += dp[i - 1];
             }
+
             if (c2 == '1' || (c2 == '2' && c1 <= '6')) {
                 dp[i] += dp[i - 2];
             }
         }
+
         return dp[n];
     }
 }
