1e4a1c90abbc0a0ddec811a71dd925075b544ef0
ChunHisangLee <goldrushkl168@gmail.com>
2023-12-25 09:14:01 +0800

    2023/12/25 91

--- editor/en/[91]Decode Ways.java
+++ editor/en/[91]Decode Ways.java
@@ -4,21 +4,26 @@ Decode Ways
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    final char ZERO = '0';
+    final char ONE = '1';
+    final char TWO = '2';
+    final char SIX = '6';
+
     public int numDecodings(String s) {
         int n = s.length();
         int[] dp = new int[n + 1];
         dp[0] = 1;
-        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        dp[1] = s.charAt(0) == ZERO ? 0 : 1;
 
         for (int i = 2; i <= n; i++) {
             char c1 = s.charAt(i - 1);
             char c2 = s.charAt(i - 2);
 
-            if (c1 != '0') {
+            if (c1 != ZERO) {
                 dp[i] += dp[i - 1];
             }
 
-            if (c2 == '1' || (c2 == '2' && c1 <= '6')) {
+            if (c2 == ONE || (c2 == TWO && c1 <= SIX)) {
                 dp[i] += dp[i - 2];
             }
         }
