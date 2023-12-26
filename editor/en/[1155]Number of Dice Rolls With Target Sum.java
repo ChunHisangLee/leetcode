3d2efe9d328b48b1e79bd8c223d66ad4a6d68212
ChunHisangLee <goldrushkl168@gmail.com>
2023-12-26 09:10:45 +0800

    2023/12/26 1155

--- editor/en/[1155]Number of Dice Rolls With Target Sum.java
+++ editor/en/[1155]Number of Dice Rolls With Target Sum.java
@@ -1,56 +1,64 @@
 1155
-        Number of Dice Rolls With Target Sum
+Number of
+Dice Rolls
+With Target
+Sum
         2023-02-27 13:43:03
 
 //leetcode submit region begin(Prohibit modification and deletion)
-/*
-還不會
- */
 class Solution {
-    int MOD = (int) 1e9 + 7;
+    final int MOD = (int) 1e9 + 7;
 
     public int numRollsToTarget(int n, int k, int target) {
-        Integer[][] dp = new Integer[n + 1][target + 1];
-        return dpFun(n, k, target, dp);
+        Integer[][] memo = new Integer[n + 1][target + 1];
+        return getCount(n, k, target, memo);
     }
 
-    public int dpFun(int n, int k, int target, Integer[][] dp) {
-        if (target < 0) {
-            return 0;
-        }
-        if (target == 0 && n == 0) {
+    private int getCount(int n, int k, int target, Integer[][] memo) {
+        if (n == 0 && target == 0) {
             return 1;
-        } else if (n == 0 || target == 0) {
+        }
+
+        if (n == 0 || target == 0) {
             return 0;
         }
-        if (dp[n][target] != null) {
-            return dp[n][target];
+
+        if (memo[n][target] != null) {
+            return memo[n][target];
         }
+
         int count = 0;
+
         for (int i = 1; i <= k; i++) {
-            count = (count + dpFun(n - 1, k, target - i, dp)) % MOD;
+            if (target >= i) {
+                count = (count + getCount(n - 1, k, target - i, memo)) % MOD;
+            }
         }
-        dp[n][target] = count;
-        return dp[n][target];
+        memo[n][target] = count;
+        return count;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 /*
 class Solution {
+    int MOD = (int) 1e9 + 7;
+
     public int numRollsToTarget(int n, int k, int target) {
-        int MOD = (int) 1e9 + 7;
         int[][] dp = new int[n + 1][target + 1];
-        dp[n][target] = 1;
-        for (int index = n - 1; index >= 0; index--) {
-            for (int sum = 0; sum <= target; sum++) {
-                int count = 0;
-                for (int i = 1; i <= Math.min(k, target - sum); i++) {
-                    count = (count + dp[index + 1][sum + i]) % MOD;
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            int sum=0;
+            for (int j = 1; j <= target; j++) {
+                for (int l = 1; l <= k; l++) {
+                    if (j - l >= 0) {
+                        dp[i][j] = (dp[i][j] + dp[i - 1][j - l]) % MOD;
+                    }
                 }
-                dp[index][sum] = count;
             }
         }
-        return dp[0][0];
+
+        return dp[n][target];
     }
 }
  */
