db95a684095cd0730090298ab87a9eaffa214bcc
Jack Lee <goldrushkl168@gmail.com>
2024-01-01 12:36:19 +0800

    2024/01/01 1066

--- editor/en/[1066]Campus Bikes II.java
+++ editor/en/[1066]Campus Bikes II.java
@@ -1,48 +1,50 @@
-1066
-        Campus Bikes II
-        2022-12-15 09:25:53
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    int memo[] = new int[1024];
 
-    private int countNumOfOnes(int mask) {
-        int count = 0;
-        while (mask != 0) {
-            mask &= (mask - 1);
-            count++;
-        }
-        return count;
+import java.util.Arrays;
+
+class Solution {
+    private static final int MAX = Integer.MAX_VALUE;
+
+    private int countSetBits(int mask) {
+        return Integer.bitCount(mask);
     }
 
-    private int findDistance(int[] worker, int[] bike) {
+    private int computeDistance(int[] worker, int[] bike) {
         return Math.abs(worker[0] - bike[0]) + Math.abs(worker[1] - bike[1]);
     }
 
-    private int minimumDistanceSum(int[][] workers, int[][] bikes) {
+    private int computeMinimumDistance(int[][] workers, int[][] bikes, int[] memo) {
         int numOfBikes = bikes.length;
         int numOfWorkers = workers.length;
-        int smallestDistanceSum = Integer.MAX_VALUE;
+        int minDistanceSum = MAX;
+        Arrays.fill(memo, MAX);
         memo[0] = 0;
+
         for (int mask = 0; mask < (1 << numOfBikes); mask++) {
-            int nextWorkerIndex = countNumOfOnes(mask);
-            if (nextWorkerIndex >= numOfWorkers) {
-                smallestDistanceSum = Math.min(smallestDistanceSum, memo[mask]);
+            int nextWorker = countSetBits(mask);
+
+            if (nextWorker >= numOfWorkers) {
+                minDistanceSum = Math.min(minDistanceSum, memo[mask]);
                 continue;
             }
+
             for (int bikeIndex = 0; bikeIndex < numOfBikes; bikeIndex++) {
                 if ((mask & (1 << bikeIndex)) == 0) {
-                    int newMask = (1 << bikeIndex) | mask;
-                    memo[newMask] = Math.min(memo[newMask], memo[mask] + findDistance(workers[nextWorkerIndex], bikes[bikeIndex]));
+                    int newMask = mask | (1 << bikeIndex);
+                    int newDistance = memo[mask] + computeDistance(workers[nextWorker], bikes[bikeIndex]);
+                    memo[newMask] = Math.min(memo[newMask], newDistance);
                 }
             }
         }
-        return smallestDistanceSum;
+
+        return minDistanceSum;
     }
 
     public int assignBikes(int[][] workers, int[][] bikes) {
-        Arrays.fill(memo, Integer.MAX_VALUE);
-        return minimumDistanceSum(workers, bikes);
+        int[] memo = new int[1 << bikes.length];
+        return computeMinimumDistance(workers, bikes, memo);
     }
 }
+
 //leetcode submit region end(Prohibit modification and deletion)
