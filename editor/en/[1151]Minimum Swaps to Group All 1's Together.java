3c3c9c5db7d7e2a1f47192ad9d542e1241a71b31
ChunHisangLee <goldrushkl168@gmail.com>
2024-01-04 17:52:05 +0800

    2024/01/04 1151

--- editor/en/[1151]Minimum Swaps to Group All 1's Together.java
+++ editor/en/[1151]Minimum Swaps to Group All 1's Together.java
@@ -2,7 +2,23 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int minSwaps(int[] data) {
-        
+        int totalOnes = Arrays.stream(data).sum();
+        int left = 0;
+        int maxOnes = 0;
+        int currOnes = 0;
+
+        for (int right = 0; right < data.length; right++) {
+            currOnes += data[right];
+
+            if (right - left + 1 > totalOnes) {
+                currOnes -= data[left];
+                left++;
+            }
+
+            maxOnes = Math.max(maxOnes, currOnes);
+        }
+
+        return totalOnes - maxOnes;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
