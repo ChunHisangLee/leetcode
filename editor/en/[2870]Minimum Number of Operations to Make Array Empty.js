8a7f0e230cefc2c2b394e1177d8ba287285dccc0
ChunHisangLee <goldrushkl168@gmail.com>
2024-01-04 09:17:17 +0800

    2024/01/04 2870

---
+++ editor/en/[2870]Minimum Number of Operations to Make Array Empty.js
@@ -0,0 +1,28 @@
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minOperations = function (nums) {
+    let map = new Map();
+    let count = 0;
+
+    for (let num of nums) {
+        map.set(num, (map.get(num) || 0) + 1);
+    }
+
+    for (let num of map.values()) {
+        if (num === 1) {
+            return -1;
+        }
+
+        if (num % 3 === 0) {
+            count += (num / 3);
+        } else {
+            count += (Math.floor(num / 3) + 1);
+        }
+    }
+
+    return count;
+};
+//leetcode submit region end(Prohibit modification and deletion)
