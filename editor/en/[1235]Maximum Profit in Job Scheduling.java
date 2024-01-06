7b59236a48ec2c3879e1b4baa639b571b7db354a
Jack Lee <goldrushkl168@gmail.com>
2024-01-06 13:57:34 +0800

    Revert "2024/01/06 1235"
    This reverts commit b7ecaadcbd39036f7467cb358c30f5809dff287e.

--- editor/en/[1235]Maximum Profit in Job Scheduling.java
+++ editor/en/[1235]Maximum Profit in Job Scheduling.java
@@ -57,7 +57,6 @@ class Job {
     }
 }
 
-
 //leetcode submit region end(Prohibit modification and deletion)
 /*
 https://leetcode.com/problems/maximum-profit-in-job-scheduling/solutions/1358899/maximum-profit-in-job-scheduling/
