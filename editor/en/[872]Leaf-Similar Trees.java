a041951b6b4be0c268f6d905cddaee17d74ff939
ChunHisangLee <goldrushkl168@gmail.com>
2024-01-09 09:01:44 +0800

    2024/01/09 872

--- editor/en/[872]Leaf-Similar Trees.java
+++ editor/en/[872]Leaf-Similar Trees.java
@@ -1,23 +1,5 @@
-872
-        Leaf-Similar Trees
-        2022-12-08 09:16:27
-//leetcode submit region begin(Prohibit modification and deletion)
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
- * }
- */
+//leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
         List<Integer> list1 = new ArrayList<>();
@@ -27,13 +9,17 @@ class Solution {
         return list1.equals(list2);
     }
 
-    private void dfs(TreeNode node, List<Integer> list) {
-        if (node != null) {
-            if (node.left == null && node.right == null)
-                list.add(node.val);
-            dfs(node.left, list);
-            dfs(node.right, list);
+    private void dfs(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
         }
+
+        if (root.left == null && root.right == null) {
+            list.add(root.val);
+        }
+
+        dfs(root.left, list);
+        dfs(root.right, list);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
