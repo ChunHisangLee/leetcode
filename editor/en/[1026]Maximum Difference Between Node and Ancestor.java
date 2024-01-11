855d833e7135e1eb5a30463ab82d288c1a4a88bf
ChunHisangLee <goldrushkl168@gmail.com>
2024-01-11 09:13:00 +0800

    2024/01/11 1026

--- editor/en/[1026]Maximum Difference Between Node and Ancestor.java
+++ editor/en/[1026]Maximum Difference Between Node and Ancestor.java
@@ -1,37 +1,20 @@
-1026
-        Maximum Difference Between Node and Ancestor
-        2022-12-01 18:04:35
+
 //leetcode submit region begin(Prohibit modification and deletion)
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
- * }
- */
 class Solution {
     public int maxAncestorDiff(TreeNode root) {
-        if (root == null)
-            return 0;
         return dfs(root, root.val, root.val);
     }
 
     private int dfs(TreeNode node, int max, int min) {
-        if (node == null)
+        if (node == null) {
             return max - min;
-        max = Math.max(max, node.val);
-        min = Math.min(min, node.val);
-        int left = dfs(node.left, max, min);
-        int right = dfs(node.right, max, min);
+        }
+
+        int currMax = Math.max(max, node.val);
+        int currMin = Math.min(min, node.val);
+        int left = dfs(node.left, currMax, currMin);
+        int right = dfs(node.right, currMax, currMin);
         return Math.max(left, right);
     }
 }
