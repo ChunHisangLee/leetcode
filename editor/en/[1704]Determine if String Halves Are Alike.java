5bf161ba5414c6d99df324e40f1189accb7e5a57
ChunHisangLee <goldrushkl168@gmail.com>
2024-01-12 09:39:28 +0800

    2024/01/12 1704

--- editor/en/[1704]Determine if String Halves Are Alike.java
+++ editor/en/[1704]Determine if String Halves Are Alike.java
@@ -1,25 +1,22 @@
-1704
-        Determine if String Halves Are Alike
-        2022-12-01 09:45:06
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean halvesAreAlike(String s) {
-        int len = s.length();
-        String vowels = "aeiouAEIOU";
-        String first = s.substring(0, len / 2);
-        String sceond = s.substring(len / 2, len);
-        int countFirst = 0;
-        for (char c : first.toCharArray()) {
-            if (vowels.indexOf(c) != -1)
-                countFirst++;
-        }
-        int countSecond = 0;
-        for (char c : sceond.toCharArray()) {
-            if (vowels.indexOf(c) != -1)
-                countSecond++;
-        }
+        int mid = s.length() / 2;
+        int countFirst = getCount(s.substring(0, mid));
+        int countSecond = getCount(s.substring(mid));
         return countFirst == countSecond;
+    }
+
+    private int getCount(String s) {
+        int count = 0;
+
+        for (char c : s.toCharArray()) {
+            if ("aeiouAEIOU".indexOf(c) != -1) {
+                count++;
+            }
+        }
+        return count;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
---
+++ editor/en/[1704]Determine if String Halves Are Alike.js
@@ -0,0 +1,25 @@
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var halvesAreAlike = function (s) {
+    const mid = s.length / 2;
+    const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    const getCount = (str, vowels) => {
+
+        let count = 0;
+        for (const c of str) {
+            if (vowels.includes(c)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    const first = getCount(s.substring(0, mid), vowels);
+    const second = getCount(s.substring(mid), vowels);
+    return first === second;
+};
+//leetcode submit region end(Prohibit modification and deletion)
