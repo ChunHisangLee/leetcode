916ae5774ac79bf6def46677f27f90d2da957f96
Jack Lee <goldrushkl168@gmail.com>
2024-01-14 10:28:56 +0800

    2024/01/14 1657

--- editor/en/[1657]Determine if Two Strings Are Close.java
+++ editor/en/[1657]Determine if Two Strings Are Close.java
@@ -1,27 +1,31 @@
-1657
-        Determine if Two Strings Are Close
-        2022-12-02 09:57:43
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean closeStrings(String word1, String word2) {
         if (word1.length() != word2.length())
             return false;
-        int[] word1Arr = new int[26];
-        int[] word2Arr = new int[26];
-        for (char c : word1.toCharArray()) {
-            word1Arr[c - 'a']++;
-        }
-        for (char c : word2.toCharArray()) {
-            word2Arr[c - 'a']++;
-        }
+        int[] arr1 = getCount(word1);
+        int[] arr2 = getCount(word2);
+
         for (int i = 0; i < 26; i++) {
-            if (word1Arr[i] == 0 && word2Arr[i] > 0 || word2Arr[i] == 0 && word1Arr[i] > 0)
+            if (arr1[i] == 0 && arr2[i] > 0 || ar1657r2[i] == 0 && arr1[i] > 0) {
                 return false;
+            }
         }
-        Arrays.sort(word1Arr);
-        Arrays.sort(word2Arr);
-        return Arrays.equals(word1Arr, word2Arr);
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        return Arrays.equals(arr1, arr2);
+    }
+
+    private int[] getCount(String s) {
+        int[] arr = new int[26];
+
+        for (char c : s.toCharArray()) {
+            arr[c - 'a']++;
+        }
+
+        return arr;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
