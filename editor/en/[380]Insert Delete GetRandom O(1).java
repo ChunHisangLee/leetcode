2b02893cf20249bcaf849b190aa7ba84ab73ca18
ChunHisangLee <goldrushkl168@gmail.com>
2024-01-15 10:38:30 +0800

    2024/01/16 380

--- editor/en/[380]Insert Delete GetRandom O(1).java
+++ editor/en/[380]Insert Delete GetRandom O(1).java
@@ -1,41 +1,82 @@
-380
-        Insert Delete GetRandom O(1)
-        2023-01-12 18:33:02
+
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class RandomizedSet {
-    Set<Integer> set;
+    Map<Integer, Integer> map;
     List<Integer> list;
-    Random rand = new Random();
+    Random random;
 
     public RandomizedSet() {
-        set = new HashSet<>();
+        map = new HashMap<>();
         list = new ArrayList<>();
+        random = new Random();
     }
 
     public boolean insert(int val) {
-        if (set.contains(val)) {
+        if (map.containsKey(val)) {
             return false;
         }
-        set.add(val);
+
+        map.put(val, list.size());
         list.add(val);
         return true;
     }
 
     public boolean remove(int val) {
-        if (!set.contains(val)) {
+        if (!map.containsKey(val)) {
             return false;
         }
-        set.remove(val);
-        list.remove(new Integer(val));
+
+        int lastIndex = list.size() - 1;
+        int lastElement = list.get(lastIndex);
+        int index = map.get(val);
+
+        if (index != lastIndex) {
+            map.put(lastElement, index);
+            list.set(index, lastElement);
+        }
+
+        map.remove(val);
+        list.remove(lastIndex);
         return true;
     }
 
     public int getRandom() {
-        int index = rand.nextInt(set.size());
-        return list.get(index);
+        return list.get(random.nextInt(list.size()));
     }
 }
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ * <p>
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ * <p>
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ * <p>
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ * <p>
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
 
 /**
  * Your RandomizedSet object will be instantiated and called as such:
