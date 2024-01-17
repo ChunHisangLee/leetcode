f0a572e291d0bec155468ca4d97ec67437c5408a
ChunHisangLee <goldrushkl168@gmail.com>
2024-01-17 09:54:02 +0800

    2024/01/17 1207

---
+++ editor/en/[380]Insert Delete GetRandom O(1).js
@@ -0,0 +1,37 @@
+//leetcode submit region begin(Prohibit modification and deletion)
+
+var RandomizedSet = function() {
+    
+};
+
+/** 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.insert = function(val) {
+    
+};
+
+/** 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.remove = function(val) {
+    
+};
+
+/**
+ * @return {number}
+ */
+RandomizedSet.prototype.getRandom = function() {
+    
+};
+
+/** 
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
+//leetcode submit region end(Prohibit modification and deletion)
