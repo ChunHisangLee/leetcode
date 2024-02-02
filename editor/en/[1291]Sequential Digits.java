7573faf61e2e15ec68404868c5907659ef39f277
ChunHisangLee <goldrushkl168@gmail.com>
2024-02-02 09:19:00 +0800

    2024/02/02 1291

--- editor/en/[1291]Sequential Digits.java
+++ editor/en/[1291]Sequential Digits.java
@@ -2,39 +2,45 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public List<Integer> sequentialDigits(int low, int high) {
-        String str = "123456789";
-        int lowLen = String.valueOf(low).length();
-        int highLen = String.valueOf(high).length();
-        int n = 10;
         List<Integer> list = new ArrayList<>();
-        for (int i = lowLen; i < highLen + 1; i++) {
-            for (int j = 0; j < n - i; j++) {
-                int num = Integer.parseInt(str.substring(j, j + i));
+        String digits = "123456789";
+
+        for (int len = 2; len <= digits.length(); len++) {
+            for (int start = 0; start <= digits.length() - len; start++) {
+                int num = Integer.parseInt(digits.substring(start, start + len));
+
                 if (num >= low && num <= high) {
                     list.add(num);
                 }
             }
         }
+
         return list;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 /*
+
+
 class Solution {
     public List<Integer> sequentialDigits(int low, int high) {
-        String str = "123456789";
-        int n = 10;
         List<Integer> list = new ArrayList<>();
-        int lowLen = String.valueOf(low).length();
-        int highLen = String.valueOf(high).length();
-        for (int i = lowLen; i < highLen + 1; i++) {
-            for (int j = 0; j < n - i; j++) {
-                int num = Integer.parseInt(str.substring(j, j + i));
+
+        for (int i = 1; i < 10; i++) {
+            int num = 0;
+            int j = i;
+            while (num <= high && j < 10) {
+                num = num * 10 + j;
+
                 if (num >= low && num <= high) {
                     list.add(num);
                 }
+
+                j++;
             }
         }
+
+        Collections.sort(list);
         return list;
     }
 }
