a18e5d22f30ad2f61be9d2c7df61b3757b3e236e
ChunHisangLee <goldrushkl168@gmail.com>
2024-02-07 11:44:23 +0800

    2024/02/07 23, 270

--- editor/en/[270]Closest Binary Search Tree Value.java
+++ editor/en/[270]Closest Binary Search Tree Value.java
@@ -1,31 +1,22 @@
-270
-        Closest Binary Search Tree Value
-        2022-12-04 22:35:21
 //leetcode submit region begin(Prohibit modification and deletion)
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
- * }
- */
+
 class Solution {
     public int closestValue(TreeNode root, double target) {
-        int res = root.val;
+        int result = root.val;
+
         while (root != null) {
-            res = Math.abs(res - target) > Math.abs(root.val - target) ? root.val : res;
-            root = root.val > target ? root.left : root.right;
+            double diffRoot = Math.abs(target - root.val);
+            double diffResult = Math.abs(target - result);
+
+            if (diffRoot < diffResult || (diffRoot == diffResult && root.val < result)) {
+                result = root.val;
+            }
+
+            root = target < root.val ? root.left : root.right;
         }
-        return res;
+
+        return result;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
