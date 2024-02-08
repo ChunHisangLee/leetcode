d81d562507fb2e22d0bead26842b9ea11e7f4400
Jack Lee <goldrushkl168@gmail.com>
2024-02-08 11:01:12 +0800

    2024/02/08 279

--- editor/en/[279]Perfect Squares.java
+++ editor/en/[279]Perfect Squares.java
@@ -2,44 +2,77 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int numSquares(int n) {
-        int dp[] = new int[n + 1];
-        for (int i = 0; i <= n; i++) {
-            dp[i] = Integer.MAX_VALUE;
+        while (n % 4 == 0) {
+            n /= 4;
         }
-        dp[0] = 0;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j * j <= i; j++) {
-                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+
+        if (n % 8 == 7) {
+            return 4;
+        }
+
+        if (this.isSquare(n)) {
+            return 1;
+        }
+
+        for (int i = 1; i * i <= n; ++i) {
+            if (this.isSquare(n - i * i)) {
+                return 2;
             }
         }
-        return dp[n];
+
+        return 3;
+    }
+
+    private boolean isSquare(int n) {
+        int sq = (int) Math.sqrt(n);
+        return n == sq * sq;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 /*
 class Solution {
+    public int numSquares(int n) {
+        // four-square and three-square theorems.
+        while (n % 4 == 0) {
+            n /= 4;
+        }
 
-  protected boolean isSquare(int n) {
-    int sq = (int) Math.sqrt(n);
-    return n == sq * sq;
-  }
+        if (n % 8 == 7) {
+            return 4;
+        }
 
-  public int numSquares(int n) {
-    // four-square and three-square theorems.
-    while (n % 4 == 0)
-      n /= 4;
-    if (n % 8 == 7)
-      return 4;
-
-    if (this.isSquare(n))
-      return 1;
-    // enumeration to check if the number can be decomposed into sum of two squares.
-    for (int i = 1; i * i <= n; ++i) {
-      if (this.isSquare(n - i * i))
-        return 2;
+        if (this.isSquare(n)) {
+            return 1;
+        }
+        // enumeration to check if the number can be decomposed into sum of two squares.
+        for (int i = 1; i * i <= n; ++i) {
+            if (this.isSquare(n - i * i)) {
+                return 2;
+            }
+        }
+        // bottom case of three-square theorem.
+        return 3;
     }
-    // bottom case of three-square theorem.
-    return 3;
-  }
+
+    private boolean isSquare(int n) {
+        int sq = (int) Math.sqrt(n);
+        return n == sq * sq;
+    }
+}
+
+class Solution {
+    public int numSquares(int n) {
+        int dp[] = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        return dp[n];
+    }
 }
  */
