377f16c9cb47bc49473e011224b1657afdfdb9bc
ChunHisangLee <goldrushkl168@gmail.com>
2024-02-10 09:34:38 +0800

    2024/02/10 5, 516, 647

--- editor/en/[5]Longest Palindromic Substring.java
+++ editor/en/[5]Longest Palindromic Substring.java
@@ -1,16 +1,13 @@
 5
-        Longest Palindromic Substring
-        2023-01-07 00:36:45
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public String longestPalindrome(String s) {
-        char[] c = s.toCharArray();
         int left = 0;
         int right = 0;
 
-        for (int i = 0; i < c.length; i++) {
-            int len = Math.max(getLen(c, i, i), getLen(c, i, i + 1));
+        for (int i = 0; i < s.length(); i++) {
+            int len = Math.max(getLen(s, i, i), getLen(s, i, i + 1));
 
             if (len > right - left) {
                 left = i - (len - 1) / 2;
@@ -21,8 +18,8 @@ class Solution {
         return s.substring(left, right + 1);
     }
 
-    private int getLen(char[] c, int left, int right) {
-        while (left >= 0 && right < c.length && c[left] == c[right]) {
+    private int getLen(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             left--;
             right++;
         }
