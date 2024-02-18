296ca838df72c302d5f67082d9916163f2c04bc5
Jack Lee <goldrushkl168@gmail.com>
2024-02-18 10:41:01 +0800

    2024/02/18 2402

--- editor/en/[2402]Meeting Rooms III.java
+++ editor/en/[2402]Meeting Rooms III.java
@@ -1,41 +1,50 @@
-2402
-        Meeting Rooms III
-        2022-12-19 13:47:43
-
-/*
-還不會
- */
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int mostBooked(int n, int[][] meetings) {
         Arrays.sort(meetings, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
         int[] count = new int[n];
         long[] room = new long[n];
+
         for (int i = 0; i < meetings.length; i++) {
-            int meetingStart = meetings[i][0];
-            int meetingEnd = meetings[i][1];
-            int nextRoom = -1;
-            long sooner = Long.MAX_VALUE;
-            for (int j = 0; j < room.length; j++) {
-                if (room[j] <= meetingStart) {
-                    nextRoom = j;
-                    break;
-                }
-                if (room[j] < sooner) {
-                    nextRoom = j;
-                    sooner = room[j];
-                }
-            }
+            int start = meetings[i][0];
+            int end = meetings[i][1];
+            int nextRoom = getAvailableRoom(start, room);
             count[nextRoom]++;
-            room[nextRoom] = Math.max(meetingEnd, meetingEnd - meetingStart + room[nextRoom]);
+            room[nextRoom] = Math.max(end, end - start + room[nextRoom]);
         }
-        int max = 0;
-        for (int i = 1; i < count.length; i++) {
-            if (count[i] > count[max]) {
-                max = i;
+
+        return getMostBookedRoom(count);
+    }
+
+    private int getAvailableRoom(int start, long[] room) {
+        long earliest = Long.MAX_VALUE;
+        int nextAvailable = -1;
+
+        for (int j = 0; j < room.length; j++) {
+            if (room[j] <= start) {
+                nextAvailable = j;
+                break;
+            }
+
+            if (room[j] < earliest) {
+                nextAvailable = j;
+                earliest = room[j];
             }
         }
-        return max;
+
+        return nextAvailable;
+    }
+
+    private int getMostBookedRoom(int[] count) {
+        int maxBooked = 0;
+
+        for (int i = 1; i < count.length; i++) {
+            if (count[i] > count[maxBooked]) {
+                maxBooked = i;
+            }
+        }
+
+        return maxBooked;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
