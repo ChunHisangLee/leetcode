43e62bbd23d2bc8350df25bea6e5177d944daba5
ChunHisangLee <goldrushkl168@gmail.com>
2024-03-11 09:05:51 +0800

    2024/03/11 791

---
+++ editor/en/[791]Custom Sort String.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String customSortString(String order, String s) {
+        StringBuilder sb = new StringBuilder();
+        int[] count = new int[26];
+
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        for (char c : order.toCharArray()) {
+            while (count[c - 'a'] > 0) {
+                sb.append(c);
+                count[c - 'a']--;
+            }
+        }
+
+        for (char c = 'a'; c <= 'z'; c++) {
+            while (count[c - 'a'] > 0) {
+                sb.append(c);
+                count[c - 'a']--;
+            }
+        }
+
+        return sb.toString();
+    }
+}
