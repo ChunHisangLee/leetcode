d4406c7d64a4f2431935bc1e123a7b1e1752583b
ChunHisangLee <goldrushkl168@gmail.com>
2024-03-15 09:20:23 +0800

    2024/03/15 238, 253

--- editor/en/[253]Meeting Rooms II.java
+++ editor/en/[253]Meeting Rooms II.java
@@ -1,19 +1,18 @@
-253
-        Meeting Rooms II
-        2022-12-19 13:47:39
-
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
         Arrays.sort(intervals, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         pq.add(intervals[0][1]);
+
         for (int i = 1; i < intervals.length; i++) {
             if (intervals[i][0] >= pq.peek()) {
                 pq.poll();
             }
+
             pq.add(intervals[i][1]);
         }
+
         return pq.size();
     }
 }
