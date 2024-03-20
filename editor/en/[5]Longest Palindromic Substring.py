ed442c53451f40fd59cd5346bfadee5cf0983f77
ChunHisangLee <goldrushkl168@gmail.com>
2024-03-20 09:53:08 +0800

    2024/03/20 5 for test

---
+++ editor/en/[5]Longest Palindromic Substring.py
@@ -0,0 +1,6 @@
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        
+# leetcode submit region end(Prohibit modification and deletion)
