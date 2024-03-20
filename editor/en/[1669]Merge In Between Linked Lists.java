470671065fd876c9bb2395bd8b92d106148188f1
ChunHisangLee <goldrushkl168@gmail.com>
2024-03-20 09:21:52 +0800

    2024/03/20  1669

--- editor/en/[1669]Merge In Between Linked Lists.java
+++ editor/en/[1669]Merge In Between Linked Lists.java
@@ -1,18 +1,29 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
-        
+        ListNode dummy = new ListNode(0);
+        dummy.next = list1;
+        ListNode prev = dummy;
+
+        for (int i = 0; i < a; i++) {
+            prev = prev.next;
+        }
+
+        ListNode end = prev;
+
+        for (int i = a; i <= b; i++) {
+            end = end.next;
+        }
+
+        prev.next = list2;
+
+        while (list2.next != null) {
+            list2 = list2.next;
+        }
+
+        list2.next = end.next;
+        return dummy.next;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
