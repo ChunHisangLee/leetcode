08d6cc5e6c04329d0c5ed880aee07635da7476d1
Jack Lee <goldrushkl168@gmail.com>
2024-04-04 13:49:24 +0800

    2024/04/04 1614

--- editor/en/[1614]Maximum Nesting Depth of the Parentheses.js
+++ editor/en/[1614]Maximum Nesting Depth of the Parentheses.js
@@ -3,15 +3,15 @@
  * @param {string} s
  * @return {number}
  */
-var maxDepth = function(s) {
-    let current=0;
-    let max=0;
+var maxDepth = function (s) {
+    let current = 0;
+    let max = 0;
 
-    for (let c of s){
-        if(c=='('){
+    for (let c of s) {
+        if (c === '(') {
             current++;
-            max=Math.max(max,current);
-        }else if(c==')'){
+            max = Math.max(max, current);
+        } else if (c === ')') {
             current--;
         }
     }
