718ce83454e1542c1d3731212409d69485ae0c5e
ChunHisangLee <goldrushkl168@gmail.com>
2024-04-18 11:47:36 +0800

    2024/04/18 463

--- editor/en/[463]Island Perimeter.java
+++ editor/en/[463]Island Perimeter.java
@@ -9,14 +9,20 @@ class Solution {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (grid[i][j] == 1) {
-                    counter += 4;
-
-                    if (i > 0 && grid[i - 1][j] == 1) {
-                        counter -= 2;
+                    if (i == 0 || grid[i - 1][j] == 0) {
+                        counter++;
                     }
 
-                    if (j > 0 && grid[i][j - 1] == 1) {
-                        counter -= 2;
+                    if (i == rows - 1 || grid[i + 1][j] == 0) {
+                        counter++;
+                    }
+
+                    if (j == 0 || grid[i][j - 1] == 0) {
+                        counter++;
+                    }
+
+                    if (j == cols - 1 || grid[i][j + 1] == 0) {
+                        counter++;
                     }
                 }
             }
