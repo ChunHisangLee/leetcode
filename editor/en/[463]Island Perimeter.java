9b11b4bdbcb0e60e28ca7c9006de40c81f40b537
ChunHisangLee <goldrushkl168@gmail.com>
2024-04-18 11:39:17 +0800

    2024/04/18 463

--- editor/en/[463]Island Perimeter.java
+++ editor/en/[463]Island Perimeter.java
@@ -2,26 +2,27 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int islandPerimeter(int[][] grid) {
-        int edge4 = 0;
-        int edge2 = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int counter = 0;
 
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 if (grid[i][j] == 1) {
-                    edge4++;
+                    counter += 4;
 
                     if (i > 0 && grid[i - 1][j] == 1) {
-                        edge2++;
+                        counter -= 2;
                     }
 
                     if (j > 0 && grid[i][j - 1] == 1) {
-                        edge2++;
+                        counter -= 2;
                     }
                 }
             }
         }
 
-        return edge4 * 4 - edge2 * 2;
+        return counter;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
