d778ea7683fd39093f78a900e62997052bdb7866
Jack Lee <goldrushkl168@gmail.com>
2024-05-11 19:25:37 +0800

    2024/05/11 724, 1991

---
+++ editor/en/[724]Find Pivot Index.java
@@ -0,0 +1,25 @@
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n = nums.length;
+        int[] prefixSumLeft = new int[n];
+        int[] prefixSumRight = new int[n];
+
+        for (int i = 1; i < n; i++) {
+            prefixSumLeft[i] = prefixSumLeft[i - 1] + nums[i - 1];
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            prefixSumRight[i] = prefixSumRight[i + 1] + nums[i + 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (prefixSumLeft[i] == prefixSumRight[i]) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
