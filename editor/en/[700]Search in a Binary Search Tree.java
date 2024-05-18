8f66f80640fd6be84012a293258efc471195d603
Jack Lee <goldrushkl168@gmail.com>
2024-05-18 14:25:04 +0800

    2024/05/18 700, 338

--- editor/en/[700]Search in a Binary Search Tree.java
+++ editor/en/[700]Search in a Binary Search Tree.java
@@ -1,29 +1,18 @@
-700
-        Search in a Binary Search Tree
-        2022-12-22 00:38:20
 //leetcode submit region begin(Prohibit modification and deletion)
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
- * }
- */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        while (root != null && root.val != val) {
-            root = root.val > val ? root.left : root.right;
+        if (root == null) {
+            return null;
         }
-        return root;
+
+        if (root.val == val) {
+            return root;
+        } else if (root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
