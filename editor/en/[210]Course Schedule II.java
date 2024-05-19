1dd1854c6f2dabdc1933cfeed2c9f63d39940bc8
Jack Lee <goldrushkl168@gmail.com>
2024-05-19 22:05:40 +0800

    2024/05/19 207, 210

--- editor/en/[210]Course Schedule II.java
+++ editor/en/[210]Course Schedule II.java
@@ -1,39 +1,49 @@
-210
-        Course Schedule II
-        2023-02-15 16:17:24
-
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         List<List<Integer>> list = new ArrayList<>();
+
         for (int i = 0; i < numCourses; i++) {
             list.add(new ArrayList<>());
         }
+
         int[] inDegree = new int[numCourses];
+
         for (int[] pre : prerequisites) {
             list.get(pre[1]).add(pre[0]);
             inDegree[pre[0]]++;
         }
-        Deque<Integer> dq = new ArrayDeque<>();
+
+        Queue<Integer> queue = new ArrayDeque<>();
+
         for (int i = 0; i < numCourses; i++) {
             if (inDegree[i] == 0) {
-                dq.add(i);
+                queue.offer(i);
             }
         }
-        int[] arr = new int[numCourses];
+
         int count = 0;
-        while (!dq.isEmpty()) {
-            int curr = dq.poll();
-            arr[count] = curr;
+        int[] result = new int[numCourses];
+
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            result[count] = curr;
             count++;
+
             for (int num : list.get(curr)) {
                 inDegree[num]--;
+
                 if (inDegree[num] == 0) {
-                    dq.add(num);
+                    queue.offer(num);
                 }
             }
         }
-        return count == numCourses ? arr : new int[0];
+
+        if (count == numCourses) {
+            return result;
+        } else {
+            return new int[0];
+        }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
