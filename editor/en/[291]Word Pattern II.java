797423c452c4491a3ddf26984da568a6d3b312b2
ChunHisangLee <goldrushkl168@gmail.com>
2024-05-23 08:32:20 +0800

    2024/05/23 291

--- editor/en/[291]Word Pattern II.java
+++ editor/en/[291]Word Pattern II.java
@@ -2,7 +2,49 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean wordPatternMatch(String pattern, String s) {
-        
+        return backtrack(pattern, 0, s, 0, new HashMap<>(), new HashSet<>());
+    }
+
+    private boolean backtrack(String pattern, int pIndex, String s, int sIndex, Map<Character, String> map, Set<String> set) {
+        if (pIndex == pattern.length() && sIndex == s.length()) {
+            return true;
+        }
+
+        if (pIndex == pattern.length() || sIndex == s.length()) {
+            return false;
+        }
+
+        char c = pattern.charAt(pIndex);
+
+        if (map.containsKey(c)) {
+            String mappedStr = map.get(c);
+
+            if (!s.startsWith(mappedStr, sIndex)) {
+                return false;
+            }
+
+            return backtrack(pattern, pIndex + 1, s, sIndex + mappedStr.length(), map, set);
+        }
+
+        for (int i = sIndex; i < s.length(); i++) {
+            String candidate = s.substring(sIndex, i + 1);
+
+            if (set.contains(candidate)) {
+                continue;
+            }
+
+            map.put(c, candidate);
+            set.add(candidate);
+
+            if (backtrack(pattern, pIndex + 1, s, sIndex + candidate.length(), map, set)) {
+                return true;
+            }
+
+            map.remove(c);
+            set.remove(candidate);
+        }
+
+        return false;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
