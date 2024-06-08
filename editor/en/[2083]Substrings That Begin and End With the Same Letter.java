f3ef582668b57d78576b5cd4555c0b14980c16dc
Jack Lee <goldrushkl168@gmail.com>
2024-06-08 18:45:36 +0800

    2024/06/08 2083

---
+++ editor/en/[2083]Substrings That Begin and End With the Same Letter.java
@@ -0,0 +1,18 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public long numberOfSubstrings(String s) {
+        int[] count = new int[26];
+        long result = 0;
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            result += count[c - 'a'] + 1;
+            count[c - 'a']++;
+        }
+
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
