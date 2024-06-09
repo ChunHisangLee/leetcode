9f37ab82dd24febd8fc54a76a69f7ddbfba8fef3
Jack Lee <goldrushkl168@gmail.com>
2024-06-09 10:00:25 +0800

    2024/06/09

--- editor/en/[974]Subarray Sums Divisible by K.java
+++ editor/en/[974]Subarray Sums Divisible by K.java
@@ -1,27 +1,46 @@
-974
-        Subarray Sums Divisible by K
-        2023-01-19 09:20:16
-
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        int n = nums.length;
-        int[] count = new int[k];
-        count[0] = 1;
-        int sum = 0;
-        int res = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int prefixSum = 0;
+        int result = 0;
+        map.put(0, 1);
+
         for (int num : nums) {
-            num %= k;
-            if (num < 0) {
-                num += k;
+            prefixSum += num;
+            int afterMod = (prefixSum % k + k) % k;
+
+            if (map.containsKey(afterMod)) {
+                result += map.get(afterMod);
             }
+
+            map.put(afterMod, map.getOrDefault(afterMod, 0) + 1);
         }
-        for (int num : nums) {
-            sum += num;
-            res += count[sum % k];
-            count[sum % k]++;
-        }
-        return res;
+        return result;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
+/*
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        int n = nums.length;
+        int[] prefixSum = new int[n];
+        prefixSum[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        }
+
+        int[] count = new int[k];
+        count[0] = 1;
+        int result = 0;
+
+        for (int sum : prefixSum) {
+            int afterMod = (sum % k + k) % k;
+            result += count[afterMod];
+            count[afterMod]++;
+        }
+        return result;
+    }
+}
+ */
