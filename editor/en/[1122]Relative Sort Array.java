c9b1d455782bbc7b66a4c056c79233f17d17193f
ChunHisangLee <goldrushkl168@gmail.com>
2024-06-11 08:43:37 +0800

    2024/06/11 1122

--- editor/en/[1122]Relative Sort Array.java
+++ editor/en/[1122]Relative Sort Array.java
@@ -3,20 +3,19 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
         Map<Integer, Integer> map = new TreeMap<>();
-        int[] arr = new int[arr1.length];
 
         for (int num : arr1) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
+        int[] arr = new int[arr1.length];
         int index = 0;
 
         for (int num : arr2) {
             int count = map.get(num);
 
-            while (count > 0) {
+            while (count-- > 0) {
                 arr[index++] = num;
-                count--;
             }
 
             map.put(num, 0);
@@ -25,9 +24,8 @@ class Solution {
         for (int key : map.keySet()) {
             int count = map.get(key);
 
-            while (count > 0) {
+            while (count-- > 0) {
                 arr[index++] = key;
-                count--;
             }
         }
 
