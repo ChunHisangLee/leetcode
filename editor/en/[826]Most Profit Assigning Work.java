f9e8e0bdd80c73a0ca8fc6fe574d51c8dddf392e
ChunHisangLee <goldrushkl168@gmail.com>
2024-06-18 13:57:44 +0800

    2024/06/17 826

--- editor/en/[826]Most Profit Assigning Work.java
+++ editor/en/[826]Most Profit Assigning Work.java
@@ -3,22 +3,19 @@
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
         int n = difficulty.length;
-        int[][] job = new int[n][2];
-
-        for (int i = 0; i < n; i++) {
-            job[i][0] = difficulty[i];
-            job[i][1] = profit[i];
-        }
-
-        Arrays.sort(job, (a, b) -> a[0] - b[0]);
+        int[][] jobs = IntStream.range(0, n)
+                .parallel()
+                .mapToObj(i -> new int[]{difficulty[i], profit[i]})
+                .sorted((a, b) -> a[0] - b[0])
+                .toArray(int[][]::new);
         Arrays.sort(worker);
         int maxProfit = 0;
         int totalProfit = 0;
         int index = 0;
 
         for (int skill : worker) {
-            while (index < n && job[index][0] <= skill) {
-                maxProfit = Math.max(maxProfit, job[index][1]);
+            while (index < n && jobs[index][0] <= skill) {
+                maxProfit = Math.max(maxProfit, jobs[index][1]);
                 index++;
             }
 
