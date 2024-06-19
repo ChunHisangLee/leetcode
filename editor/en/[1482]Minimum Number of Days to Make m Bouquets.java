e3f3cfd0509794a6e1a3d7b175f64b340eb66945
ChunHisangLee <goldrushkl168@gmail.com>
2024-06-19 10:12:53 +0800

    2024/06/19 1482

---
+++ editor/en/[1482]Minimum Number of Days to Make m Bouquets.java
@@ -0,0 +1,50 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+        if (bloomDay.length / k < m) {
+            return -1;
+        }
+
+        int left = Integer.MAX_VALUE;
+        int right = Integer.MIN_VALUE;
+
+        for (int day : bloomDay) {
+            left = Math.min(left, day);
+            right = Math.max(right, day);
+        }
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (canMakeBouquets(bloomDay, m, k, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    private boolean canMakeBouquets(int[] bloomDay, int m, int k, int days) {
+        int bouquets = 0;
+        int flowers = 0;
+
+        for (int day : bloomDay) {
+            if (day <= days) {
+                flowers++;
+
+                if (flowers == k) {
+                    bouquets++;
+                    flowers = 0;
+                }
+            } else {
+                flowers = 0;
+            }
+        }
+
+        return bouquets >= m;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
