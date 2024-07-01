61098c137505270b1d67d0978316c0645e32e547
ChunHisangLee <goldrushkl168@gmail.com>
2024-07-01 09:07:21 +0800

    2024/07/01 1101, 1550

--- editor/en/[1101]The Earliest Moment When Everyone Become Friends.java
+++ editor/en/[1101]The Earliest Moment When Everyone Become Friends.java
@@ -1,35 +1,70 @@
-1101
-        The Earliest Moment When Everyone Become Friends
-        2023-02-03 12:27:32
-
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int earliestAcq(int[][] logs, int n) {
-        int[] arr = new int[n];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        for (int i = 0; i < n; i++) {
-            arr[i] = i;
-        }
+        Arrays.sort(logs, (a, b) -> a[0] - b[0]);
+        UnionFind uf = new UnionFind(n);
+
         for (int[] log : logs) {
-            pq.add(log);
-        }
-        while (!pq.isEmpty()) {
-            int[] pair = pq.poll();
-            int x = find(arr, pair[1]);
-            int y = find(arr, pair[2]);
-            if (x != y) {
-                arr[x] = y;
-                n--;
-            }
-            if (n == 1) {
-                return pair[0];
+            int timestamp = log[0];
+            int x = log[1];
+            int y = log[2];
+            uf.union(x, y);
+
+            if (uf.getCount() == 1) {
+                return timestamp;
             }
         }
+
         return -1;
     }
+}
 
-    private int find(int[] arr, int i) {
-        return arr[i] == i ? i : (arr[i] = find(arr, arr[i]));
+class UnionFind {
+    private int[] parent;
+    private int[] rank;
+    private int count;
+
+    public UnionFind(int n) {
+        parent = new int[n];
+        rank = new int[n];
+        count = n;
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    public int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+
+        return parent[x] = find(parent[x]);
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX == rootY) {
+            return;
+        }
+
+        if (rank[rootX] > rank[rootY]) {
+            parent[rootY] = rootX;
+        } else if (rank[rootX] < rank[rootY]) {
+            parent[rootX] = rootY;
+        } else {
+            parent[rootY] = rootX;
+            rank[rootX]++;
+        }
+
+        count--;
+    }
+
+    public int getCount() {
+        return count;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
