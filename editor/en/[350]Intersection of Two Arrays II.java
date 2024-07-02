d5c79f423fe3f2abc6585d25aa0bfa7e02266a47
ChunHisangLee <goldrushkl168@gmail.com>
2024-07-02 08:39:19 +0800

    2024/07/02 350

--- editor/en/[350]Intersection of Two Arrays II.java
+++ editor/en/[350]Intersection of Two Arrays II.java
@@ -1,29 +1,23 @@
-350
-        Intersection of Two Arrays II
-        2022-12-30 10:02:50
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         Map<Integer, Integer> map = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
+
         for (int num : nums1) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
+
+        List<Integer> list = new ArrayList<>();
+
         for (int num : nums2) {
-            if (map.containsKey(num)) {
+            if (map.containsKey(num) && map.get(num) > 0) {
                 list.add(num);
                 map.put(num, map.get(num) - 1);
-                if (map.get(num) == 0) {
-                    map.remove(num);
-                }
             }
         }
-        int[] arr = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            arr[i] = list.get(i);
-        }
-        return arr;
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
