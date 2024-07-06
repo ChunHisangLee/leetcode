f510048ca2a141c65de7cfc893beb6babe37d137
Jack Lee <goldrushkl168@gmail.com>
2024-07-06 10:23:35 +0800

    2024/07/06 1894

--- editor/en/[1894]Find the Student that Will Replace the Chalk.java
+++ editor/en/[1894]Find the Student that Will Replace the Chalk.java
@@ -1,4 +1,22 @@
 //leetcode submit region begin(Prohibit modification and deletion)
+
+class Solution {
+    public int chalkReplacer(int[] chalk, int k) {
+        int n = chalk.length;
+        long[] prefixSum = new long[n];
+        prefixSum[0] = chalk[0];
+        IntStream.range(1, n).forEach(i -> prefixSum[i] = prefixSum[i - 1] + chalk[i]);
+        final int adjustedK = (int) (k % prefixSum[n - 1]);
+        int result = IntStream.range(0, n)
+                .filter(i -> prefixSum[i] > adjustedK)
+                .findFirst()
+                .orElse(-1);
+
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+/*
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
         int n = chalk.length;
@@ -26,4 +44,4 @@ class Solution {
         return left;
     }
 }
-//leetcode submit region end(Prohibit modification and deletion)
+ */
