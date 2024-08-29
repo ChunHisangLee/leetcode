2d99b49b428379990d286a1e49a97c9555de64df
Jack Lee <goldrushkl168@gmail.com>
2024-08-29 16:57:15 +0800

    2024/08/29 666

--- editor/en/[666]Path Sum IV.java
+++ editor/en/[666]Path Sum IV.java
@@ -1,42 +1,20 @@
 //leetcode submit region begin(Prohibit modification and deletion)
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public int pathSum(int[] nums) {
-        Map<Integer, Integer> treeMap = new HashMap<>();
-
-        for (int num : nums) {
-            int depth = num / 100;
-            int position = (num / 10) % 10;
-            int value = num % 10;
-            int key = depth * 10 + position;
-            treeMap.put(key, value);
-        }
-
-        return dfs(treeMap, nums[0] / 100, (nums[0] / 10) % 10, 0);
-    }
-
-    private int dfs(Map<Integer, Integer> treeMap, int depth, int position, int currentSum) {
-        int key = depth * 10 + position;
-        currentSum += treeMap.get(key);
-        int leftChildKey = (depth + 1) * 10 + (2 * position - 1);
-        int rightChildKey = (depth + 1) * 10 + (2 * position);
-        boolean hasLeft = treeMap.containsKey(leftChildKey);
-        boolean hasRight = treeMap.containsKey(rightChildKey);
-
-        if (!hasLeft && !hasRight) {
-            return currentSum;
-        }
-
+        int[][] pathCounts = new int[5][9];
         int totalSum = 0;
 
-        if (hasLeft) {
-            totalSum += dfs(treeMap, depth + 1, 2 * position - 1, currentSum);
-        }
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int depth = nums[i] / 100;
+            int position = (nums[i] / 10) % 10;
+            int value = nums[i] % 10;
+            pathCounts[depth][position] = Math.max(pathCounts[depth][position], 1);
+            totalSum += pathCounts[depth][position] * value;
 
-        if (hasRight) {
-            totalSum += dfs(treeMap, depth + 1, 2 * position, currentSum);
+            if (depth > 1) {
+                int parentPosition = (position + 1) / 2;
+                pathCounts[depth - 1][parentPosition] += pathCounts[depth][position];
+            }
         }
 
         return totalSum;
