c3814c6e9277b246f0a26b9548ff1bbaf1ef4675
Jack Lee <goldrushkl168@gmail.com>
2024-08-29 16:50:36 +0800

    2024/08/29 666

--- editor/en/[666]Path Sum IV.java
+++ editor/en/[666]Path Sum IV.java
@@ -1,10 +1,45 @@
-666
-Path Sum IV
-2023-02-24 14:42:02
 //leetcode submit region begin(Prohibit modification and deletion)
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int pathSum(int[] nums) {
-        
+        Map<Integer, Integer> treeMap = new HashMap<>();
+
+        for (int num : nums) {
+            int depth = num / 100;
+            int position = (num / 10) % 10;
+            int value = num % 10;
+            int key = depth * 10 + position;
+            treeMap.put(key, value);
+        }
+
+        return dfs(treeMap, nums[0] / 100, (nums[0] / 10) % 10, 0);
+    }
+
+    private int dfs(Map<Integer, Integer> treeMap, int depth, int position, int currentSum) {
+        int key = depth * 10 + position;
+        currentSum += treeMap.get(key);
+        int leftChildKey = (depth + 1) * 10 + (2 * position - 1);
+        int rightChildKey = (depth + 1) * 10 + (2 * position);
+        boolean hasLeft = treeMap.containsKey(leftChildKey);
+        boolean hasRight = treeMap.containsKey(rightChildKey);
+
+        if (!hasLeft && !hasRight) {
+            return currentSum;
+        }
+
+        int totalSum = 0;
+
+        if (hasLeft) {
+            totalSum += dfs(treeMap, depth + 1, 2 * position - 1, currentSum);
+        }
+
+        if (hasRight) {
+            totalSum += dfs(treeMap, depth + 1, 2 * position, currentSum);
+        }
+
+        return totalSum;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
