3f1f46312baccb439bd6f538db62dfac275b51a8
Jack Lee <goldrushkl168@gmail.com>
2024-11-04 23:35:27 +0800

    2024/11/04 721

--- editor/en/[721]Accounts Merge.java
+++ editor/en/[721]Accounts Merge.java
@@ -5,8 +5,10 @@ class Solution {
         int n = accounts.size();
         UnionFind uf = new UnionFind(n);
         Map<String, Integer> map = new HashMap<>();
+
         for (int i = 0; i < n; i++) {
             List<String> list = accounts.get(i);
+
             for (int j = 1; j < list.size(); j++) {
                 String email = list.get(j);
                 if (!map.containsKey(email)) {
@@ -16,21 +18,28 @@ class Solution {
                 }
             }
         }
+
         Map<Integer, List<String>> idToEmail = new HashMap<>();
+
         for (String key : map.keySet()) {
             int id = uf.find(map.get(key));
+
             if (!idToEmail.containsKey(id)) {
                 idToEmail.put(id, new ArrayList<>());
             }
+
             idToEmail.get(id).add(key);
         }
+
         List<List<String>> res = new ArrayList<>();
+
         for (Integer id : idToEmail.keySet()) {
             List<String> email = idToEmail.get(id);
             Collections.sort(email);
             email.add(0, accounts.get(id).get(0));
             res.add(email);
         }
+
         return res;
     }
 }
@@ -40,6 +49,7 @@ class UnionFind {
 
     public UnionFind(int n) {
         group = new int[n];
+
         for (int i = 0; i < n; i++) {
             group[i] = i;
         }
@@ -52,10 +62,10 @@ class UnionFind {
     public void union(int i, int j) {
         int x = find(i);
         int y = find(j);
+
         if (x != y) {
             group[x] = y;
         }
     }
 }
-
 //leetcode submit region end(Prohibit modification and deletion)
