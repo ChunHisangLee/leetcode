97dea34982d03c7393a60c98be206c0050ba875a
Jack Lee <goldrushkl168@gmail.com>
2024-12-01 10:23:37 +0800

    2024/12/01 1346

--- editor/en/[1346]Check If N and Its Double Exist.java
+++ editor/en/[1346]Check If N and Its Double Exist.java
@@ -1,18 +1,21 @@
-1346
-        Check If N and Its Double Exist
-        2023-01-17 23:06:13
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        int n = arr.length;
-        for(int i = 0 ; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(i != j && arr[i] == 2 * arr[j]) {
-                    return true;
-                }
+        Set<Integer> seen = new HashSet<>();
+
+        for (int x : arr) {
+            if (seen.contains(2 * x)) {
+                return true;
             }
+
+            if (x % 2 == 0 && seen.contains(x / 2)) {
+                return true;
+            }
+
+            seen.add(x);
         }
+
         return false;
     }
 }
