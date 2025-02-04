8c774043dd5900a353a69b6f4181c1801fca6238
Jack Lee <goldrushkl168@gmail.com>
2025-02-04 10:03:14 +0800

    2025/02/04 change format

--- editor/en/leetcode/editor/en/[1568]Minimum Number of Days to Disconnect Island.java
+++ editor/en/leetcode/editor/en/[1568]Minimum Number of Days to Disconnect Island.java
@@ -1,61 +1,61 @@
-//leetcode submit region begin(Prohibit modification and deletion)
+// leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public int minDays(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+  public int minDays(int[][] grid) {
+    int m = grid.length;
+    int n = grid[0].length;
 
-        if (countIslands(grid) != 1) {
-            return 0;
-        }
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    grid[i][j] = 0;
-
-                    if (countIslands(grid) != 1) {
-                        return 1;
-                    }
-
-                    grid[i][j] = 1;
-                }
-            }
-        }
-
-        return 2;
+    if (countIslands(grid) != 1) {
+      return 0;
     }
 
-    private int countIslands(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        boolean[][] visited = new boolean[m][n];
-        int count = 0;
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (grid[i][j] == 1) {
+          grid[i][j] = 0;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1 && !visited[i][j]) {
-                    dfs(grid, visited, i, j);
-                    count++;
-                }
-            }
+          if (countIslands(grid) != 1) {
+            return 1;
+          }
+
+          grid[i][j] = 1;
         }
-
-        return count;
+      }
     }
 
-    private void dfs(int[][] grid, boolean[][] visited, int x, int y) {
-        int m = grid.length;
-        int n = grid[0].length;
+    return 2;
+  }
 
-        if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] == 0 || visited[x][y]) {
-            return;
+  private int countIslands(int[][] grid) {
+    int m = grid.length;
+    int n = grid[0].length;
+    boolean[][] visited = new boolean[m][n];
+    int count = 0;
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (grid[i][j] == 1 && !visited[i][j]) {
+          dfs(grid, visited, i, j);
+          count++;
         }
-
-        visited[x][y] = true;
-        dfs(grid, visited, x + 1, y);
-        dfs(grid, visited, x - 1, y);
-        dfs(grid, visited, x, y + 1);
-        dfs(grid, visited, x, y - 1);
+      }
     }
+
+    return count;
+  }
+
+  private void dfs(int[][] grid, boolean[][] visited, int x, int y) {
+    int m = grid.length;
+    int n = grid[0].length;
+
+    if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] == 0 || visited[x][y]) {
+      return;
+    }
+
+    visited[x][y] = true;
+    dfs(grid, visited, x + 1, y);
+    dfs(grid, visited, x - 1, y);
+    dfs(grid, visited, x, y + 1);
+    dfs(grid, visited, x, y - 1);
+  }
 }
-//leetcode submit region end(Prohibit modification and deletion)
+// leetcode submit region end(Prohibit modification and deletion)
--- editor/en/leetcode/editor/en/[1937]Maximum Number of Points with Cost.java
+++ editor/en/leetcode/editor/en/[1937]Maximum Number of Points with Cost.java
@@ -1,36 +1,36 @@
-//leetcode submit region begin(Prohibit modification and deletion)
+// leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
 
-    public long maxPoints(int[][] points) {
-        int n = points[0].length;
-        long[] dp = new long[n];
-        long[] newDp = new long[n];
+  public long maxPoints(int[][] points) {
+    int n = points[0].length;
+    long[] dp = new long[n];
+    long[] newDp = new long[n];
 
-        for (int[] row : points) {
-            long curMax = 0;
+    for (int[] row : points) {
+      long curMax = 0;
 
-            for (int c = 0; c < n; c++) {
-                curMax = Math.max(curMax - 1, newDp[c]);
-                dp[c] = curMax;
-            }
+      for (int c = 0; c < n; c++) {
+        curMax = Math.max(curMax - 1, newDp[c]);
+        dp[c] = curMax;
+      }
 
-            curMax = 0;
+      curMax = 0;
 
-            for (int c = n - 1; c >= 0; c--) {
-                curMax = Math.max(curMax - 1, newDp[c]);
-                dp[c] = Math.max(dp[c], curMax) + row[c];
-            }
+      for (int c = n - 1; c >= 0; c--) {
+        curMax = Math.max(curMax - 1, newDp[c]);
+        dp[c] = Math.max(dp[c], curMax) + row[c];
+      }
 
-            newDp = dp;
-        }
-
-        long result = 0;
-
-        for (int c = 0; c < n; c++) {
-            result = Math.max(result, newDp[c]);
-        }
-
-        return result;
+      newDp = dp;
     }
+
+    long result = 0;
+
+    for (int c = 0; c < n; c++) {
+      result = Math.max(result, newDp[c]);
+    }
+
+    return result;
+  }
 }
-//leetcode submit region end(Prohibit modification and deletion)
+// leetcode submit region end(Prohibit modification and deletion)
