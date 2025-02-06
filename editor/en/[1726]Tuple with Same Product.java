9dc8359457cdcb7ec0700652c2b9be9a358bb2a2
ChunHisangLee <goldrushkl168@gmail.com>
2025-02-06 14:28:59 +0800

    2025/02/06 1726

---
+++ editor/en/[1726]Tuple with Same Product.java
@@ -0,0 +1,26 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int tupleSameProduct(int[] nums) {
+        Map<Integer, Integer> productCount = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int product = nums[i] * nums[j];
+                productCount.put(product, productCount.getOrDefault(product, 0) + 1);
+            }
+        }
+
+        int result = 0;
+
+        for (int count : productCount.values()) {
+            if (count > 1) {
+                result += 8 * (count * (count - 1) / 2);
+            }
+        }
+
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
