426760620e95670ba69cd07eb9e900ce0bbf3923
Jack Lee <goldrushkl168@gmail.com>
2025-04-09 18:06:31 +0800

    2025/04/09

--- editor/en/[57]Insert Interval.java
+++ editor/en/[57]Insert Interval.java
@@ -1,23 +1,29 @@
-
-//leetcode submit region begin(Prohibit modification and deletion)
+// leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> list = new ArrayList<>();
+  public int[][] insert(int[][] intervals, int[] newInterval) {
+    List<int[]> result = new ArrayList<>();
+    int n = intervals.length;
+    int index = 0;
 
-        for (int[] interval : intervals) {
-            if (interval[1] < newInterval[0]) {
-                list.add(interval);
-            } else if (interval[0] > newInterval[1]) {
-                list.add(newInterval);
-                newInterval = interval;
-            } else {
-                newInterval[0] = Math.min(newInterval[0], interval[0]);
-                newInterval[1] = Math.max(newInterval[1], interval[1]);
-            }
-        }
-
-        list.add(newInterval);
-        return list.toArray(new int[list.size()][]);
+    while (index < n && intervals[index][1] < newInterval[0]) {
+      result.add(intervals[index]);
+      index++;
     }
+
+    while (index < n && intervals[index][0] <= newInterval[1]) {
+      newInterval[0] = Math.min(newInterval[0], intervals[index][0]);
+      newInterval[1] = Math.max(newInterval[1], intervals[index][1]);
+      index++;
+    }
+
+    result.add(newInterval);
+
+    while (index < n) {
+      result.add(intervals[index]);
+      index++;
+    }
+
+    return result.toArray(new int[result.size()][]);
+  }
 }
-//leetcode submit region end(Prohibit modification and deletion)
+// leetcode submit region end(Prohibit modification and deletion)
