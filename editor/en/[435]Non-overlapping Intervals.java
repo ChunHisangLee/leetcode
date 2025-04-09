3a7b6ac4e9e5ccefa7fae25781e58e055a392178
Jack Lee <goldrushkl168@gmail.com>
2025-04-10 00:04:39 +0800

    2025/04/09

--- editor/en/[435]Non-overlapping Intervals.java
+++ editor/en/[435]Non-overlapping Intervals.java
@@ -1,18 +1,19 @@
-
-//leetcode submit region begin(Prohibit modification and deletion)
+// leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public int eraseOverlapIntervals(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
-        int end = Integer.MIN_VALUE;
-        int count = 0;
-        for (int[] arr : intervals) {
-            if (arr[0] >= end) {
-                end = arr[1];
-            } else {
-                count++;
-            }
-        }
-        return count;
+  public int eraseOverlapIntervals(int[][] intervals) {
+    Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+    int n = intervals.length;
+    int end = intervals[0][1];
+    int count = 1;
+
+    for (int i = 0; i < n; i++) {
+      if (intervals[i][0] >= end) {
+        end = intervals[i][1];
+        count++;
+      }
     }
+
+    return n-count;
+  }
 }
-//leetcode submit region end(Prohibit modification and deletion)
+// leetcode submit region end(Prohibit modification and deletion)
