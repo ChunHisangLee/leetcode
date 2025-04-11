40a45367adb72ca8adf18e8fabb0d331754e99e9
ChunHisangLee <goldrushkl168@gmail.com>
2025-04-11 21:27:44 +0800

    2025/04/11

---
+++ editor/en/leetcode/editor/en/[133]Clone Graph.java
@@ -0,0 +1,78 @@
+// leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+  public Node cloneGraph(Node node) {
+    if (node == null) {
+      return null;
+    }
+
+    Map<Node, Node> map = new HashMap<>();
+    return cloneDFS(node, map);
+  }
+
+  private Node cloneDFS(Node node, Map<Node, Node> map) {
+    if (map.containsKey(node)) {
+      return map.get(node);
+    }
+
+    Node cloneNode = new Node(node.val);
+    map.put(node, cloneNode);
+
+    for (Node neighbor : node.neighbors) {
+      cloneNode.neighbors.add(cloneDFS(neighbor, map));
+    }
+
+    return cloneNode;
+  }
+}
+// leetcode submit region end(Prohibit modification and deletion)
+/*
+class Solution {
+  public Node cloneGraph(Node node) {
+    if (node == null) {
+      return null;
+    }
+
+    Map<Node, Node> map = new HashMap<>();
+    Deque<Node> deque = new ArrayDeque<>();
+    deque.offer(node);
+    Node cloneNode = new Node(node.val);
+    map.put(node, cloneNode);
+
+    while (!deque.isEmpty()) {
+      Node current = deque.poll();
+
+      for (Node neighbor : current.neighbors) {
+        if (!map.containsKey(neighbor)) {
+          deque.offer(neighbor);
+          Node cloneNeighbor = new Node(neighbor.val);
+          map.put(neighbor, cloneNeighbor);
+        }
+
+        map.get(current).neighbors.add(map.get(neighbor));
+      }
+    }
+
+    return cloneNode;
+  }
+}
+ */
