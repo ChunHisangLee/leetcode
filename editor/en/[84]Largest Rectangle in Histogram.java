eec0c6bc1646114858472c5cd6fce0ce28737ff1
Jack Lee <goldrushkl168@gmail.com>
2025-04-13 01:26:30 +0800

    2025/04/13

--- editor/en/[84]Largest Rectangle in Histogram.java
+++ editor/en/[84]Largest Rectangle in Histogram.java
@@ -1,28 +1,14 @@
-84
-        Largest Rectangle in Histogram
-        2022-12-26 00:18:14
-
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public int largestRectangleArea(int[] heights) {
-        Deque<Integer> dq = new ArrayDeque<>();
-        dq.push(-1);
-        int n = heights.length;
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            while ((dq.peek() != -1) && (heights[dq.peek()] >= heights[i])) {
-                int currH = heights[dq.pop()];
-                int currW = i - dq.peek() - 1;
-                max = Math.max(max, currH * currW);
-            }
-            dq.push(i);
-        }
-        while (dq.peek() != -1) {
-            int currH = heights[dq.pop()];
-            int currW = n - dq.peek() - 1;
-            max = Math.max(max, currH * currW);
-        }
-        return max;
-    }
+  public int largestRectangleArea(int[] heights) {
+      int n=heights;
+      int maxArea = 0;
+      Deque<Integer>deque = new ArrayDeque<>();
+      int[]nextLarge=new int[n];
+
+      for(int i = 0; i < n; i++) {
+
+      }
+  }
 }
 //leetcode submit region end(Prohibit modification and deletion)
