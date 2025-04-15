068c00d643a9c0c74646a32a5466f19798a54521
Jack Lee <goldrushkl168@gmail.com>
2025-04-15 23:53:40 +0800

    2025/04/15

--- editor/en/[143]Reorder List.java
+++ editor/en/[143]Reorder List.java
@@ -1,66 +1,45 @@
-//leetcode submit region begin(Prohibit modification and deletion)
+// leetcode submit region begin(Prohibit modification and deletion)
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    public void reorderList(ListNode head) {
-        if (head == null || head.next == null) {
-            return;
-        }
-
-        ListNode middle = findMiddle(head);
-        ListNode end = reverseList(middle.next);
-        middle.next = null;
-        merge(head, end);
+  public void reorderList(ListNode head) {
+    if (head == null || head.next == null) {
+      return;
     }
 
-    private ListNode findMiddle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
+    ListNode fast = head;
+    ListNode slow = head;
 
-        while (fast.next != null && fast.next.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-
-        return slow;
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
     }
 
-    private ListNode reverseList(ListNode head) {
-        ListNode prev = null;
+    ListNode secondHalf = reverseList(slow);
+    ListNode firstHalf = head;
 
-        while (head != null) {
-            ListNode temp = head.next;
-            head.next = prev;
-            prev = head;
-            head = temp;
-        }
+    while (secondHalf.next != null) {
+      ListNode temp1 = firstHalf.next;
+      ListNode temp2 = secondHalf.next;
 
-        return prev;
+      firstHalf.next = secondHalf;
+      firstHalf = temp1;
+
+      secondHalf.next = firstHalf;
+      secondHalf = temp2;
+    }
+  }
+
+  private ListNode reverseList(ListNode curr) {
+    ListNode prev = null;
+
+    while (curr != null) {
+      ListNode next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
     }
 
-    private void merge(ListNode first, ListNode second) {
-        while (first != null && second != null) {
-            ListNode tmp1 = first.next;
-            ListNode tmp2 = second.next;
-            first.next = second;
-
-            if (tmp1 == null) {
-                break;
-            }
-
-            second.next = tmp1;
-            first = tmp1;
-            second = tmp2;
-        }
-    }
+    return prev;
+  }
 }
-//leetcode submit region end(Prohibit modification and deletion)
+// leetcode submit region end(Prohibit modification and deletion)
--- editor/en/[24]Swap Nodes in Pairs.java
+++ editor/en/[24]Swap Nodes in Pairs.java
@@ -1,50 +1,28 @@
-24
-        Swap Nodes in Pairs
-        2022-11-29 13:40:42
-//leetcode submit region begin(Prohibit modification and deletion)
-
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode swapPairs(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-        ListNode first = head;
-        ListNode second = head.next;
-        first.next = swapPairs(second.next);
-        second.next = first;
-        return second;
-    }
-}
-//leetcode submit region end(Prohibit modification and deletion)
-/*
-Iterative Approach
+// leetcode submit region begin(Prohibit modification and deletion)
 
 class Solution {
-    public ListNode swapPairs(ListNode head) {
-        if (head == null || head.next == null)
-            return head;
-        ListNode dummy = head.next;
-        ListNode prev = null;
-        while (head != null && head.next != null) {
-            if (prev != null)
-                prev.next = head.next;
-            prev = head;
-            ListNode nextNode = head.next.next;
-            head.next.next = head;
-            head.next = nextNode;
-            head = nextNode;
-        }
-        return dummy;
+  public ListNode swapPairs(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
     }
+
+    ListNode dummy = new ListNode(0);
+    dummy.next = head;
+    ListNode prev = dummy;
+    ListNode first = head;
+
+    while (first != null && first.next != null) {
+      ListNode second = first.next;
+
+      prev.next = second;
+      first.next = second.next;
+      second.next = first;
+
+      prev = first;
+      first = first.next;
+    }
+
+    return dummy.next;
+  }
 }
- */
+// leetcode submit region end(Prohibit modification and deletion)
