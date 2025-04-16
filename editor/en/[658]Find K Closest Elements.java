85eed1481f89159a7a696efa9041bad32edb81f5
Jack Lee <goldrushkl168@gmail.com>
2025-04-16 21:49:29 +0800

    2025/04/16

--- editor/en/[658]Find K Closest Elements.java
+++ editor/en/[658]Find K Closest Elements.java
@@ -1,46 +1,26 @@
-658
-        Find K Closest Elements
-        2022-12-09 23:57:06
+// leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+  public List<Integer> findClosestElements(int[] arr, int k, int x) {
+    int left = 0;
+    int right = arr.length - k;
 
-//leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int left = 0;
-        int right = arr.length - 1 - k;
-        while (left <= right) {
-            int mid = (left + right) >> 1;
-            if (x - arr[mid] > arr[mid + k] - x) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < k; i++) {
-            list.add(arr[i + left]);
-        }
-        return list;
+    while (left < right) {
+      int mid = left + (right - left) / 2;
+
+      if (x - arr[mid] > arr[mid + k] - x) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
     }
-}
-//leetcode submit region end(Prohibit modification and deletion)
-/*
-class Solution {
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int left = 0;
-        int right = arr.length - 1;
-        while (right - left >= k) {
-            int mid = (left + right) >> 1;
-            if (Math.abs(arr[right] - x) < Math.abs(arr[left] - x)) {
-                left++;
-            } else {
-                right--;
-            }
-        }
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < k; i++) {
-            list.add(arr[i + left]);
-        }
-        return list;
+
+    List<Integer> result = new ArrayList<>();
+
+    for (int i = left; i < left + k; i++) {
+      result.add(arr[i]);
     }
+
+    return result;
+  }
 }
- */
+// leetcode submit region end(Prohibit modification and deletion)
