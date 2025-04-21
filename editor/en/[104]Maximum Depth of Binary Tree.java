36f17ce14ecfcc35a1359eec5135b03a78842b4f
Jack Lee <goldrushkl168@gmail.com>
2025-04-22 00:13:40 +0800

    2025/04/21

--- editor/en/[104]Maximum Depth of Binary Tree.java
+++ editor/en/[104]Maximum Depth of Binary Tree.java
@@ -1,13 +1,33 @@
-//leetcode submit region begin(Prohibit modification and deletion)
+// leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public int maxDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
+  public int maxDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+
+    Deque<TreeNode> queue = new ArrayDeque<>();
+    int depth = 0;
+    queue.offer(root);
+
+    while (!queue.isEmpty()) {
+      int size = queue.size();
+
+      for (int i = 0; i < size; i++) {
+        TreeNode node = queue.poll();
+
+        if (node.left != null) {
+          queue.offer(node.left);
         }
 
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        return Math.max(left, right) + 1;
+        if (node.right != null) {
+          queue.offer(node.right);
+        }
+      }
+
+      depth++;
     }
+
+    return depth;
+  }
 }
-//leetcode submit region end(Prohibit modification and deletion)
+// leetcode submit region end(Prohibit modification and deletion)
--- editor/en/[98]Validate Binary Search Tree.java
+++ editor/en/[98]Validate Binary Search Tree.java
@@ -1,24 +1,21 @@
-98
-Validate Binary
-Search Tree
-        2022-12-23 13:51:39
+
 //leetcode submit region begin(Prohibit modification and deletion)
 
 public class Solution {
-    public boolean isValidBST(TreeNode root) {
-        return isBinaryTree(root, Long.MIN_VALUE, Long.MAX_VALUE);
+  public boolean isValidBST(TreeNode root) {
+    return checkValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+  }
+
+  private boolean checkValid(TreeNode node, long min, long max) {
+    if (node == null) {
+      return true;
     }
 
-    private boolean isBinaryTree(TreeNode node, long min, long max) {
-        if (node == null) {
-            return true;
-        }
-
-        if (node.val <= min || node.val >= max) {
-            return false;
-        }
-
-        return isBinaryTree(node.left, min, node.val) && isBinaryTree(node.right, node.val, max);
+    if (node.val <= min || node.val >= max) {
+      return false;
     }
+
+    return checkValid(node.left, min, node.val) && checkValid(node.right, node.val, max);
+  }
 }
-//leetcode submit region end(Prohibit modification and deletion)
+// leetcode submit region end(Prohibit modification and deletion)
