058b1d44e4e4b0bb895ad4b1ba7a564121af764d
ChunHisangLee <goldrushkl168@gmail.com>
2025-04-25 16:48:06 +0800

    2025/04/25

--- editor/leetcode/editor/en/[338]Counting Bits.java
+++ editor/leetcode/editor/en/[338]Counting Bits.java
@@ -1,7 +1,7 @@
 // leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
   public int[] countBits(int n) {
-    int dp[] = new int[n + 1];
+    int[] dp = new int[n + 1];
 
     for (int i = 1; i <= n; i++) {
       dp[i] = dp[i >> 1] + (i & 1);
