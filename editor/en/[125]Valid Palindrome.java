36e963466ca63b10f46f16a2510f41536c76baeb
Jack Lee <goldrushkl168@gmail.com>
2025-05-01 22:20:34 +0800

    2025/05/01

--- editor/en/[125]Valid Palindrome.java
+++ editor/en/[125]Valid Palindrome.java
@@ -1,8 +1,26 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public boolean isPalindrome(String s) {
-        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
-        return s.equals(new StringBuilder(s).reverse().toString());
+  public boolean isPalindrome(String s) {
+    int left = 0;
+    int right = s.length() - 1;
+
+    while (left < right) {
+      while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+        left++;
+      }
+      while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+        right--;
+      }
+
+      if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+        return false;
+      }
+
+      left++;
+      right--;
     }
+
+    return true;
+  }
 }
-//leetcode submit region end(Prohibit modification and deletion)
+// leetcode submit region end(Prohibit modification and deletion)
