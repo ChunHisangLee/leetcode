212dbe3a447fe1db45712a6b5b2f1419bb2ca380
Jack Lee <goldrushkl168@gmail.com>
2025-05-06 21:35:46 +0800

    2025/05/06 home

--- editor/en/[15]3Sum.java
+++ editor/en/[15]3Sum.java
@@ -2,38 +2,68 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    List<List<Integer>> res = new ArrayList<>();
+  public List<List<Integer>> threeSum(int[] nums) {
+    Arrays.sort(nums);
+    return kSum(nums, 0, 3, 0);
+  }
 
-    public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        for (int i = 0; i < n - 2 && nums[i] <= 0; i++) {
-            if (i == 0 || nums[i] != nums[i - 1]) {
-                twoSum(nums, i, i + 1, n - 1);
-            }
+  private List<List<Integer>> kSum(int[] nums, int target, int k, int start) {
+    List<List<Integer>> result = new ArrayList<>();
+    int n = nums.length;
+
+    if (k == 2) {
+      int left = start;
+      int right = n - 1;
+
+      while (left < right) {
+        int sum = nums[left] + nums[right];
+
+        if (sum < target) {
+          left++;
+        } else if (sum > target) {
+          right--;
+        } else {
+          result.add(Arrays.asList(nums[left], nums[right]));
+          left++;
+          right--;
+
+          while (left < right && nums[left] == nums[left - 1]) {
+            left++;
+          }
+
+          while (left < right && nums[right] == nums[right + 1]) {
+            right--;
+          }
         }
-        return res;
+      }
+
+      return result;
     }
 
-    public void twoSum(int[] nums, int i, int left, int right) {
-        while (left < right) {
-            int sum = nums[i] + nums[left] + nums[right];
-            if (sum == 0) {
-                res.add(List.of(nums[i], nums[left], nums[right]));
-                left++;
-                right--;
-                while (left < right && nums[left] == nums[left - 1]) {
-                    left++;
-                }
-                while (left < right && nums[right] == nums[right + 1]) {
-                    right--;
-                }
-            } else if (sum < 0) {
-                left++;
-            } else {
-                right--;
-            }
-        }
+    for (int i = start; i < n - (k - 1); i++) {
+      if (i > start && nums[i] == nums[i - 1]) {
+        continue;
+      }
+
+      if (nums[i] * k > target) {
+        break;
+      }
+
+      if (nums[n - 1] * k < target) {
+        break;
+      }
+
+      List<List<Integer>> subResult = kSum(nums, target - nums[i], k - 1, i + 1);
+
+      for (List<Integer> subset : subResult) {
+        List<Integer> tuple = new ArrayList<>();
+        tuple.add(nums[i]);
+        tuple.addAll(subset);
+        result.add(tuple);
+      }
     }
+
+    return result;
+  }
 }
-//leetcode submit region end(Prohibit modification and deletion)
+// leetcode submit region end(Prohibit modification and deletion)
