ae91b482a053d48dd2693ad4cdedd63bbc7f639b
Jack Lee <goldrushkl168@gmail.com>
2025-05-11 22:30:00 +0800

    2025/05/11 home

--- editor/en/[1550]Three Consecutive Odds.java
+++ editor/en/[1550]Three Consecutive Odds.java
@@ -1,22 +1,21 @@
-
-//leetcode submit region begin(Prohibit modification and deletion)
+// leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public boolean threeConsecutiveOdds(int[] arr) {
-        int oddCount = 0;
+  public boolean threeConsecutiveOdds(int[] arr) {
+    int oddCount = 0;
 
-        for (int num : arr) {
-            if (num % 2 == 1) {
-                oddCount++;
-            } else {
-                oddCount = 0;
-            }
+    for (int num : arr) {
+      if (num % 2 == 1) {
+        oddCount++;
+      } else {
+        oddCount = 0;
+      }
 
-            if (oddCount == 3) {
-                return true;
-            }
-        }
-
-        return false;
+      if (oddCount == 3) {
+        return true;
+      }
     }
+
+    return false;
+  }
 }
-//leetcode submit region end(Prohibit modification and deletion)
+// leetcode submit region end(Prohibit modification and deletion)
--- editor/en/[207]Course Schedule.java
+++ editor/en/[207]Course Schedule.java
@@ -1,43 +1,39 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> list = new ArrayList<>();
-
-        for (int i = 0; i < numCourses; i++) {
-            list.add(new ArrayList<>());
-        }
-
-        int[] inDegree = new int[numCourses];
-
-        for (int[] pre : prerequisites) {
-            list.get(pre[1]).add(pre[0]);
-            inDegree[pre[0]]++;
-        }
-
-        Queue<Integer> queue = new ArrayDeque<>();
-
-        for (int i = 0; i < numCourses; i++) {
-            if (inDegree[i] == 0) {
-                queue.offer(i);
-            }
-        }
-
-        int count = 0;
-
-        while (!queue.isEmpty()) {
-            int curr = queue.poll();
-            count++;
-
-            for (int num : list.get(curr)) {
-                inDegree[num]--;
-
-                if (inDegree[num] == 0) {
-                    queue.offer(num);
-                }
-            }
-        }
-
-        return count == numCourses;
+  public boolean canFinish(int numCourses, int[][] prerequisites) {
+    List<List<Integer>> adj = new ArrayList<>();
+    for (int i = 0; i < numCourses; i++) {
+      adj.add(new ArrayList<>());
     }
+
+    int[] inDegree = new int[numCourses];
+    for (int[] pre : prerequisites) {
+      int course = pre[0];
+      int before = pre[1];
+      adj.get(before).add(course);
+      inDegree[course]++;
+    }
+
+    Queue<Integer> queue = new ArrayDeque<>();
+    for (int i = 0; i < numCourses; i++) {
+      if (inDegree[i] == 0) {
+        queue.offer(i);
+      }
+    }
+
+    int count = 0;
+    while (!queue.isEmpty()) {
+      int u = queue.poll();
+      count++;
+      for (int v : adj.get(u)) {
+        inDegree[v]--;
+        if (inDegree[v] == 0) {
+          queue.offer(v);
+        }
+      }
+    }
+
+    return count == numCourses;
+  }
 }
 // leetcode submit region end(Prohibit modification and deletion)
