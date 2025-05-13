bb50858a3166b9c11f0f500b5f827ce06d0bc4b3
Jack Lee <goldrushkl168@gmail.com>
2025-05-13 23:54:39 +0800

    2025/05/13 home

--- editor/en/[238]Product of Array Except Self.java
+++ editor/en/[238]Product of Array Except Self.java
@@ -10,7 +10,7 @@ class Solution {
     }
 
     int postfix = 1;
-    for (int i = n - 1; i >= n; i++) {
+    for (int i = n - 1; i >= 0; i--) {
       answer[i] *= postfix;
       postfix *= nums[i];
     }
