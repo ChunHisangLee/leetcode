a558b96f1f229327d155e903ff416daeb30c72b8
Jack Lee <goldrushkl168@gmail.com>
2025-05-13 23:53:05 +0800

    2025/05/13 home

--- editor/en/[238]Product of Array Except Self.java
+++ editor/en/[238]Product of Array Except Self.java
@@ -1,21 +1,21 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] answer = new int[n];
-        int postfixSum = 1;
-        answer[0] = 1;
+  public int[] productExceptSelf(int[] nums) {
+    int n = nums.length;
+    int[] answer = new int[n];
+    answer[0] = 1;
 
-        for (int i = 1; i < n; i++) {
-            answer[i] = answer[i - 1] * nums[i - 1];
-        }
-
-        for (int i = n - 2; i >= 0; i--) {
-            postfixSum *= nums[i + 1];
-            answer[i] *= postfixSum;
-        }
-
-        return answer;
+    for (int i = 1; i < n; i++) {
+      answer[i] = answer[i - 1] * nums[i - 1];
     }
+
+    int postfix = 1;
+    for (int i = n - 1; i >= n; i++) {
+      answer[i] *= postfix;
+      postfix *= nums[i];
+    }
+
+    return answer;
+  }
 }
-//leetcode submit region end(Prohibit modification and deletion)
+// leetcode submit region end(Prohibit modification and deletion)
