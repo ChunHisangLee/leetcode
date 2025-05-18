ffafd24a18fea699e293c129e2bd113a5a383f51
Jack Lee <goldrushkl168@gmail.com>
2025-05-18 23:23:03 +0800

    236, 981 - home

--- editor/en/[236]Lowest Common Ancestor of a Binary Tree.java
+++ editor/en/[236]Lowest Common Ancestor of a Binary Tree.java
@@ -1,33 +1,22 @@
-236
-        Lowest Common Ancestor of a Binary Tree
-        2022-12-01 16:45:39
 //leetcode submit region begin(Prohibit modification and deletion)
-
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        TreeNode node = dfs(root, p, q);
-        return node;
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    if (root == null) {
+      return null;
     }
 
-    public TreeNode dfs(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || root == p || root == q) {
-            return root;
-        }
-        TreeNode left = dfs(root.left, p, q);
-        TreeNode right = dfs(root.right, p, q);
-        if (left != null && right != null) {
-            return root;
-        }
-        return left == null ? right : left;
+    if (root == p || root == q) {
+      return root;
     }
+
+    TreeNode left = lowestCommonAncestor(root.left, p, q);
+    TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+    if (left != null && right != null) {
+      return root;
+    }
+
+    return left != null ? left : right;
+  }
 }
-//leetcode submit region end(Prohibit modification and deletion)
+// leetcode submit region end(Prohibit modification and deletion)
