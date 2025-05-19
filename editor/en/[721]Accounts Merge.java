dd637c2f4f27db61ede77ee878a1260fa313c303
Jack Lee <goldrushkl168@gmail.com>
2025-05-19 11:43:29 +0800

    721 - home

--- editor/en/[721]Accounts Merge.java
+++ editor/en/[721]Accounts Merge.java
@@ -1,71 +1,92 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        int n = accounts.size();
-        UnionFind uf = new UnionFind(n);
-        Map<String, Integer> map = new HashMap<>();
+  public List<List<String>> accountsMerge(List<List<String>> accounts) {
+    Map<String, Integer> emailToId = new HashMap<>();
+    Map<String, String> emailToName = new HashMap<>();
+    int idCounter = 0;
 
-        for (int i = 0; i < n; i++) {
-            List<String> list = accounts.get(i);
-
-            for (int j = 1; j < list.size(); j++) {
-                String email = list.get(j);
-                if (!map.containsKey(email)) {
-                    map.put(email, i);
-                } else {
-                    uf.union(i, map.get(email));
-                }
-            }
+    for (List<String> account : accounts) {
+      String name = account.get(0);
+      for (int i = 1; i < account.size(); i++) {
+        String email = account.get(i);
+        if (!emailToId.containsKey(email)) {
+          emailToId.put(email, idCounter);
+          idCounter++;
+          emailToName.put(email, name);
         }
-
-        Map<Integer, List<String>> idToEmail = new HashMap<>();
-
-        for (String key : map.keySet()) {
-            int id = uf.find(map.get(key));
-
-            if (!idToEmail.containsKey(id)) {
-                idToEmail.put(id, new ArrayList<>());
-            }
-
-            idToEmail.get(id).add(key);
-        }
-
-        List<List<String>> res = new ArrayList<>();
-
-        for (Integer id : idToEmail.keySet()) {
-            List<String> email = idToEmail.get(id);
-            Collections.sort(email);
-            email.add(0, accounts.get(id).get(0));
-            res.add(email);
-        }
-
-        return res;
+      }
     }
+
+    UnionFind uf = new UnionFind(idCounter);
+    for (List<String> account : accounts) {
+      int firstId = emailToId.get(account.get(1));
+
+      for (int i = 2; i < account.size(); i++) {
+        uf.union(firstId, emailToId.get(account.get(i)));
+      }
+    }
+
+    Map<Integer, List<String>> groups = new HashMap<>();
+    for (String email : emailToId.keySet()) {
+      int root = uf.find(emailToId.get(email));
+      if (!groups.containsKey(root)) {
+        groups.put(root, new ArrayList<>());
+      }
+      groups.get(root).add(email);
+    }
+
+    List<List<String>> merged = new ArrayList<>();
+    for (List<String> emailList : groups.values()) {
+      Collections.sort(emailList);
+      String name = emailToName.get(emailList.get(0));
+      List<String> account = new ArrayList<>();
+      account.add(name);
+      account.addAll(emailList);
+      merged.add(account);
+    }
+
+    return merged;
+  }
+
+  private static class UnionFind {
+    private final int[] parent;
+    private final int[] rank;
+
+    UnionFind(int n) {
+      parent = new int[n];
+      rank = new int[n];
+      for (int i = 0; i < n; i++) {
+        parent[i] = i;
+        rank[i] = 1;
+      }
+    }
+
+    int find(int x) {
+      if (parent[x] != x) {
+        parent[x] = find(parent[x]);
+      }
+
+      return parent[x];
+    }
+
+    void union(int x, int y) {
+      int rootX = find(x);
+      int rootY = find(y);
+
+      if (rootX == rootY) {
+        return;
+      }
+
+      if (rank[rootX] < rank[rootY]) {
+        parent[rootX] = rootY;
+      } else if (rank[rootX] > rank[rootY]) {
+        parent[rootY] = rootX;
+      } else {
+        parent[rootY] = rootX;
+        rank[rootX]++;
+      }
+    }
+  }
 }
-
-class UnionFind {
-    int[] group;
-
-    public UnionFind(int n) {
-        group = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            group[i] = i;
-        }
-    }
-
-    public int find(int x) {
-        return group[x] == x ? x : (group[x] = find(group[x]));
-    }
-
-    public void union(int i, int j) {
-        int x = find(i);
-        int y = find(j);
-
-        if (x != y) {
-            group[x] = y;
-        }
-    }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+// leetcode submit region end(Prohibit modification and deletion)
