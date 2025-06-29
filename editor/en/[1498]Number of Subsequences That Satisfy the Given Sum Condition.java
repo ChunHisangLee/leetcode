6462be2e3d59af9c12169eccf0afeeb85a8fd3a9
Jack Lee <goldrushkl168@gmail.com>
2025-06-29 09:34:38 +0800

    1498 - home

--- editor/en/[1498]Number of Subsequences That Satisfy the Given Sum Condition.java
+++ editor/en/[1498]Number of Subsequences That Satisfy the Given Sum Condition.java
@@ -2,17 +2,20 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int numSubseq(int[] nums, int target) {
-        final int MOD = (int) 1e9 + 7;
+        final int MOD = 1_000_000_007;
         Arrays.sort(nums);
         int n = nums.length;
         int[] arr = new int[n];
         arr[0] = 1;
+
         for (int i = 1; i < n; i++) {
             arr[i] = arr[i - 1] * 2 % MOD;
         }
+
         int count = 0;
         int left = 0;
         int right = n - 1;
+
         while (left <= right) {
             if (nums[left] + nums[right] <= target) {
                 count = (count + arr[right - left]) % MOD;
@@ -21,6 +24,7 @@ class Solution {
                 right--;
             }
         }
+
         return count;
     }
 }
