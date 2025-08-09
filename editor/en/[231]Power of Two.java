77e5206e03b7ef31b542cc3bdc96d19851053ef4
jack <goldrushkl168@gmail.com>
2025-08-09 10:35:27 +0800

    231

--- editor/en/[231]Power of Two.java
+++ editor/en/[231]Power of Two.java
@@ -1,11 +1,7 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if (n <= 0) {
-            return false;
-        }
-
-        return (n & (n - 1)) == 0;
+        return n > 0 && (n & (n - 1)) == 0;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
